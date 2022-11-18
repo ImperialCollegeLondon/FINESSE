28b4e6e590449ec496559e96be0464571b4ca014
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-18 17:33:48 +0000

    Put code for creating main window into own class
    This will make it easier for when we want to add multiple panels to the
    window.

--- finesse/__main__.py
+++ finesse/__main__.py
@@ -1,27 +1,18 @@
 """The main entry point to FINESSE."""
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication
 
-from .gui.serial_view import SerialPortControl
+from .gui.main_window import MainWindow
 
 
 def main() -> None:
     """Run FINESSE."""
     app = QApplication(sys.argv)
 
-    window = QMainWindow()
-    devices = {
-        "ST10": {"port": "COM5", "baud_rate": "9600"},
-        "DP9800": {"port": "COM1", "baud_rate": "9600"},
-    }
-    serial_port = SerialPortControl(
-        devices,
-        ("COM1", "COM5", "COM7"),
-        ("600", "9600", "115200"),
-    )
-    window.setCentralWidget(serial_port)
+    window = MainWindow()
     window.show()
+
     app.exec()
 
 
