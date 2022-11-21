cb6fbe15b4c693e6d3fa6123322b9132316d3e80
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-21 11:32:05 +0000

    Add panel for stepper motor control
    The panel contains buttons for moving the mirror to a given position.
    
    I have not added the text boxes labelled ST10 Tx and Rx from the old
    program as it was not obvious what they do.
    
    Closes #32.

--- finesse/gui/main_window.py
+++ finesse/gui/main_window.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QGridLayout, QGroupBox, QMainWindow, QWidget
 
 from .opus_view import OPUSControl
 from .serial_view import SerialPortControl
+from .stepper_motor_view import StepperMotorControl
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +16,10 @@ class MainWindow(QMainWindow):
 
         layout = QGridLayout()
 
+        # Setup for stepper motor control
+        stepper_motor = StepperMotorControl()
+
+        # Setup for serial port control
         devices = {
             "ST10": {"port": "COM5", "baud_rate": "9600"},
             "DP9800": {"port": "COM1", "baud_rate": "9600"},
@@ -24,8 +29,10 @@ class MainWindow(QMainWindow):
             ("COM1", "COM5", "COM7"),
             ("600", "9600", "115200"),
         )
+
         opus: QGroupBox = OPUSControl("127.0.0.1")
 
+        layout.addWidget(stepper_motor, 0, 0)
         layout.addWidget(serial_port, 3, 0)
         layout.addWidget(opus, 0, 1)
 
