f0d075eff10312834300c83a0b88a94a4607c243
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-22 16:26:19 +0000

    StepperMotorControl: Change colour of last-clicked button
    This is what the old program does and it seems like a useful feature.

--- finesse/gui/stepper_motor_view.py
+++ finesse/gui/stepper_motor_view.py
@@ -1,4 +1,6 @@
 """Code for controlling the stepper motor which moves the mirror."""
+from typing import Optional
+
 from pubsub import pub
 from PySide6.QtWidgets import QGridLayout, QGroupBox, QPushButton, QSpinBox
 
@@ -18,8 +20,10 @@ class StepperMotorControl(QGroupBox):
         home = self._create_stepper_button("HOME")
         self.angle = QSpinBox()
         self.angle.setMaximum(359)
-        goto = QPushButton("GOTO")
-        goto.clicked.connect(self._goto_clicked)  # type: ignore
+        self.goto = QPushButton("GOTO")
+        self.goto.clicked.connect(self._goto_clicked)  # type: ignore
+
+        self.last_clicked: Optional[QPushButton] = None
 
         layout.addWidget(zenith, 0, 0)
         layout.addWidget(nadir, 0, 1)
@@ -27,7 +31,7 @@ class StepperMotorControl(QGroupBox):
         layout.addWidget(cold_bb, 0, 3)
         layout.addWidget(home, 1, 0)
         layout.addWidget(self.angle, 1, 2)
-        layout.addWidget(goto, 1, 3)
+        layout.addWidget(self.goto, 1, 3)
 
         self.setLayout(layout)
 
@@ -36,9 +40,17 @@ class StepperMotorControl(QGroupBox):
         btn.clicked.connect(lambda: self._preset_clicked(btn))  # type: ignore
         return btn
 
+    def _set_last_clicked(self, btn: QPushButton) -> None:
+        if self.last_clicked:
+            self.last_clicked.setStyleSheet("")
+        btn.setStyleSheet("background-color: blue")
+        self.last_clicked = btn
+
     def _preset_clicked(self, btn: QPushButton) -> None:
+        self._set_last_clicked(btn)
         pub.sendMessage("stepper.move", step=btn.text().lower())
 
     def _goto_clicked(self) -> None:
         """Move stepper motor to specified position."""
+        self._set_last_clicked(self.goto)
         pub.sendMessage("stepper.move", step=self.angle.value())
