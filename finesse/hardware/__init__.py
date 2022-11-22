f4d55e926086dee138c16b621c11f43a535c2180
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-22 14:29:56 +0000

    StepperMotorControl: Move stepper motor when GOTO clicked

--- finesse/hardware/__init__.py
+++ finesse/hardware/__init__.py
@@ -1,1 +1,5 @@
 """This module contains code for interfacing with different hardware devices."""
+from .dummy_stepper_motor import DummyStepperMotor
+
+# TODO: Replace with a real stepper motor device
+stepper = DummyStepperMotor(360)
