2179918147f56135a07981ac6541aaad2bd26b2b
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-22 16:10:00 +0000

    DummyStepperMotor: Allow for moving to several preset angles

--- tests/test_stepper_motor.py
+++ tests/test_stepper_motor.py
@@ -3,6 +3,8 @@ import pytest
 
 from finesse.hardware.dummy_stepper_motor import DummyStepperMotor
 
+PRESETS = ("zenith", "nadir", "hot_bb", "cold_bb", "home")
+
 
 def test_constructor() -> None:
     """Check that the constructor only accepts values in permissible range."""
@@ -13,12 +15,12 @@ def test_constructor() -> None:
         DummyStepperMotor(-1)
 
     # Should work
-    DummyStepperMotor(1)
+    DummyStepperMotor(len(PRESETS))
 
 
-def test_move_to() -> None:
+def test_move_to_number() -> None:
     """Check that the stepper motor can be moved to valid positions."""
-    stepper = DummyStepperMotor(2)
+    stepper = DummyStepperMotor(len(PRESETS))
 
     # Check that we start at step 0
     assert stepper.current_step == 0
@@ -27,8 +29,21 @@ def test_move_to() -> None:
     with pytest.raises(ValueError):
         stepper.move_to(-1)
     with pytest.raises(ValueError):
-        stepper.move_to(2)
+        stepper.move_to(len(PRESETS))
 
     # Check that we can move to a valid position
     stepper.move_to(1)
     assert stepper.current_step == 1
+
+
+def test_move_to_preset() -> None:
+    """Check that the stepper motor can be moved to valid preset values."""
+    stepper = DummyStepperMotor(len(PRESETS))
+
+    # Check that we get an error for invalid presets
+    with pytest.raises(ValueError):
+        stepper.move_to("MADE UP")
+
+    # Check that we don't get error for valid presets
+    for name in PRESETS:
+        stepper.move_to(name)
