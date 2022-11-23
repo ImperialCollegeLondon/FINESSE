1a88c622b5aec34b76a2326ac342e5ef22877502
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-23 17:02:55 +0000

    Use pytest's parametrize to check ranges of input values

--- tests/test_stepper_motor.py
+++ tests/test_stepper_motor.py
@@ -1,48 +1,53 @@
 """Tests for DummyStepperMotor."""
+from itertools import chain
+
 import pytest
 
 from finesse.config import ANGLE_PRESETS
 from finesse.hardware.dummy_stepper_motor import DummyStepperMotor
 
 
-def test_constructor() -> None:
-    """Check that the constructor only accepts values in permissible range."""
+@pytest.mark.parametrize("steps", (len(ANGLE_PRESETS), 1000, 10000000))
+def test_constructor_good(steps: int) -> None:
+    """Check valid arguments to constructor."""
+    DummyStepperMotor(steps)
+
+
+@pytest.mark.parametrize("steps", (-1000000, -1000, *range(-1, len(ANGLE_PRESETS))))
+def test_constructor_bad(steps: int) -> None:
+    """Check invalid arguments to constructor."""
     with pytest.raises(ValueError):
-        DummyStepperMotor(0)
-
-    with pytest.raises(ValueError):
-        DummyStepperMotor(-1)
-
-    # Should work
-    DummyStepperMotor(len(ANGLE_PRESETS))
+        DummyStepperMotor(steps)
 
 
-def test_move_to_number() -> None:
-    """Check that the stepper motor can be moved to valid positions."""
-    stepper = DummyStepperMotor(360)
-
-    # Check that we start at step 0
+@pytest.mark.parametrize("target", range(36))
+def test_move_to_number_good(target: int) -> None:
+    """Check move_to for valid numbers."""
+    stepper = DummyStepperMotor(36)
     assert stepper.current_step == 0
 
-    # Out-of-range arguments
+    stepper.move_to(10.0 * float(target))
+    assert stepper.current_step == target
+
+
+@pytest.mark.parametrize("target", chain(range(-1, -36, -1), range(36, 2 * 36)))
+def test_move_to_number_bad(target: int) -> None:
+    """Check move_to for invalid numbers."""
+    stepper = DummyStepperMotor(36)
+    assert stepper.current_step == 0
+
     with pytest.raises(ValueError):
-        stepper.move_to(-1.0)
+        stepper.move_to(10.0 * float(target))
+
+
+@pytest.mark.parametrize("name", ANGLE_PRESETS)
+def test_move_to_preset_good(name: str) -> None:
+    """Check move_to for valid preset names."""
+    DummyStepperMotor(360).move_to(name)
+
+
+@pytest.mark.parametrize("name", ("", "ZENITH", "kevin", "badger"))
+def test_move_to_preset_bad(name: str) -> None:
+    """Check move_to for invalid preset names."""
     with pytest.raises(ValueError):
-        stepper.move_to(360.0)
-
-    # Check that we can move to a valid position
-    stepper.move_to(1.0)
-    assert stepper.current_step == 1
-
-
-def test_move_to_preset() -> None:
-    """Check that the stepper motor can be moved to valid preset values."""
-    stepper = DummyStepperMotor(360)
-
-    # Check that we get an error for invalid presets
-    with pytest.raises(ValueError):
-        stepper.move_to("MADE UP")
-
-    # Check that we don't get error for valid presets
-    for name in ANGLE_PRESETS:
-        stepper.move_to(name)
+        DummyStepperMotor(360).move_to(name)
