15f61408b859657b5645fd98d6d0584daabce93b
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2022-11-23 05:56:24 +0000

    Add OPUS to main window

--- finesse/gui/main_window.py
+++ finesse/gui/main_window.py
@@ -1,6 +1,7 @@
 """Code for FINESSE's main GUI window."""
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QGridLayout, QGroupBox, QMainWindow, QWidget
 
+from .opus_view import OPUSControl
 from .serial_view import SerialPortControl
 
 
@@ -12,13 +13,22 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("FINESSE")
 
+        layout = QGridLayout()
+
         devices = {
             "ST10": {"port": "COM5", "baud_rate": "9600"},
             "DP9800": {"port": "COM1", "baud_rate": "9600"},
         }
-        serial_port = SerialPortControl(
+        serial_port: QGroupBox = SerialPortControl(
             devices,
             ("COM1", "COM5", "COM7"),
             ("600", "9600", "115200"),
         )
-        self.setCentralWidget(serial_port)
+        opus: QGroupBox = OPUSControl()
+
+        layout.addWidget(serial_port, 3, 0)
+        layout.addWidget(opus, 0, 1)
+
+        central = QWidget()
+        central.setLayout(layout)
+        self.setCentralWidget(central)
