38f869692ca1fd3918506aa9a669830398bcde5c
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-23 16:21:16 +0000

    Bundle all buttons for moving mirror into group
    This makes the code terser and more expressive.

--- finesse/gui/stepper_motor_view.py
+++ finesse/gui/stepper_motor_view.py
@@ -1,8 +1,12 @@
 """Code for controlling the stepper motor which moves the mirror."""
-from typing import Optional
-
 from pubsub import pub
-from PySide6.QtWidgets import QGridLayout, QGroupBox, QPushButton, QSpinBox
+from PySide6.QtWidgets import (
+    QButtonGroup,
+    QGridLayout,
+    QGroupBox,
+    QPushButton,
+    QSpinBox,
+)
 
 from ..config import ANGLE_PRESETS
 
@@ -16,51 +20,40 @@ class StepperMotorControl(QGroupBox):
 
         layout = QGridLayout()
 
+        # Bundle all the buttons for moving the mirror into one group
+        self.button_group = QButtonGroup()
+        self.button_group.buttonClicked.connect(self._preset_clicked)  # type: ignore
+
+        # Add all the buttons for preset positions
         BUTTONS_PER_ROW = 4
         for i, preset in enumerate(ANGLE_PRESETS):
-            btn = self._create_stepper_button(preset.upper())
+            btn = self._add_checkable_button(preset.upper())
+            self.button_group.addButton(btn)
+
             row, col = divmod(i, BUTTONS_PER_ROW)
             layout.addWidget(btn, row, col)
 
+        # We also have a way for users to move the mirror to an angle of their choice
         self.angle = QSpinBox()
         self.angle.setMaximum(359)
-        self.goto = QPushButton("GOTO")
-        self.goto.clicked.connect(self._goto_clicked)  # type: ignore
+        self.goto = self._add_checkable_button("GOTO")
 
         layout.addWidget(self.angle, 1, 2)
         layout.addWidget(self.goto, 1, 3)
 
         self.setLayout(layout)
 
-        self.last_clicked: Optional[QPushButton] = None
-
-    def _create_stepper_button(self, name: str) -> QPushButton:
-        """Create a button to move the motor to a preset position.
-
-        Args:
-            name: The name of the preset (will be converted to lowercase)
-        """
+    def _add_checkable_button(self, name: str) -> QPushButton:
+        """Add a selectable button to button_group."""
         btn = QPushButton(name)
-        btn.clicked.connect(lambda: self._preset_clicked(btn))  # type: ignore
+        btn.setCheckable(True)
+        btn.setStyleSheet("QPushButton::checked{background: blue; color: white;}")
+
+        self.button_group.addButton(btn)
+
         return btn
-
-    def _set_selected_button(self, btn: QPushButton) -> None:
-        """Change the currently selected button, indicated by colour.
-
-        Args:
-            btn: The just-clicked button
-        """
-        if self.last_clicked:
-            self.last_clicked.setStyleSheet("")
-        btn.setStyleSheet("background-color: blue")
-        self.last_clicked = btn
 
     def _preset_clicked(self, btn: QPushButton) -> None:
         """Move the stepper motor to preset position."""
-        self._set_selected_button(btn)
-        pub.sendMessage("stepper.move", target=btn.text().lower())
-
-    def _goto_clicked(self) -> None:
-        """Move stepper motor to specified position."""
-        self._set_selected_button(self.goto)
-        pub.sendMessage("stepper.move", target=float(self.angle.value()))
+        target = float(self.angle.value()) if btn is self.goto else btn.text().lower()
+        pub.sendMessage("stepper.move", target=target)
