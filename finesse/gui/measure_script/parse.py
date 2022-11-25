46043799d8b1c77d31bac20b7b84a6b08456ded2
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-25 15:57:42 +0000

    Move code for loading scripts with error messages to parse.py

--- finesse/gui/measure_script/parse.py
+++ finesse/gui/measure_script/parse.py
@@ -1,11 +1,23 @@
 """Code for parsing the YAML-formatted measure scripts."""
+from dataclasses import dataclass
 from io import TextIOBase
-from typing import Any, Dict, Union
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
 
 import yaml
+from PySide6.QtWidgets import QWidget
 from schema import And, Or, Schema, SchemaError
 
 from ...config import ANGLE_PRESETS
+from ..error_message import show_error_message
+
+
+@dataclass
+class Script:
+    """Represents a measure script, including its file path and data."""
+
+    path: Path
+    measurements: Dict[str, Any]
 
 
 class ParseError(Exception):
@@ -14,6 +26,25 @@ class ParseError(Exception):
     def __init__(_) -> None:
         """Create a new ParseError."""
         super().__init__("Error parsing measure script")
+
+
+def try_load_script(parent: QWidget, file_path: Path) -> Optional[Script]:
+    """Try to load a measure script at the specified path.
+
+    Args:
+        parent: The parent widget (for error messages shown)
+        file_path: The path to the script to be loaded
+    Returns:
+        A Script if successful, else None
+    """
+    try:
+        with open(file_path, "r") as f:
+            return Script(file_path, parse_script(f))
+    except OSError as e:
+        show_error_message(parent, f"Error: Could not read {file_path}: {str(e)}")
+    except ParseError:
+        show_error_message(parent, f"Error: {file_path} is in an invalid format")
+    return None
 
 
 def parse_script(script: Union[str, TextIOBase]) -> Dict[str, Any]:
