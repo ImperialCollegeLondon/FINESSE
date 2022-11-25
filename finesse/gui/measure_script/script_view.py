4ae585e0c619cc9749701e3c15e3ea4ac0017623
Alex Dewar <a.dewar@imperial.ac.uk>
2022-11-25 12:20:45 +0000

    Allow user to edit scripts

--- finesse/gui/measure_script/script_view.py
+++ finesse/gui/measure_script/script_view.py
@@ -1,7 +1,8 @@
 """Contains a panel for loading and editing measure scripts."""
+from pathlib import Path
 from typing import Optional
 
-from PySide6.QtWidgets import QGroupBox, QHBoxLayout, QPushButton
+from PySide6.QtWidgets import QFileDialog, QGroupBox, QHBoxLayout, QPushButton
 
 from .script_edit_dialog import ScriptEditDialog
 
@@ -14,16 +15,30 @@ class ScriptControl(QGroupBox):
         super().__init__("Script control")
 
         create_btn = QPushButton("Create new script")
-        create_btn.clicked.connect(self._create_btn_clicked)  # type: ignore
+        create_btn.clicked.connect(self._show_edit_dialog)  # type: ignore
+
+        edit_btn = QPushButton("Edit script")
+        edit_btn.clicked.connect(self._edit_btn_clicked)  # type: ignore
 
         layout = QHBoxLayout()
         layout.addWidget(create_btn)
-
+        layout.addWidget(edit_btn)
         self.setLayout(layout)
 
         self.dialog: Optional[ScriptEditDialog] = None
 
-    def _create_btn_clicked(self) -> None:
+    def _show_edit_dialog(self, file_path: Optional[Path] = None) -> None:
+        """Create and show edit dialog if it doesn't exist."""
         if not self.dialog or self.dialog.isHidden():
-            self.dialog = ScriptEditDialog(self.window())
+            self.dialog = ScriptEditDialog(self.window(), file_path)
             self.dialog.show()
+
+    def _edit_btn_clicked(self) -> None:
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, caption="Choose script file", dir=str(Path.home()), filter="*.yaml"
+        )
+
+        # TODO: What happens if user clicks edit when dialog is open?
+        # TODO: Handle IO/parsing errors
+        if file_path:
+            self._show_edit_dialog(Path(file_path))
