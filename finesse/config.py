8d8bac9529a9a3ce3c53aff8e4a576564e4b129b
Alex Dewar <a.dewar@imperial.ac.uk>
2023-01-11 16:04:44 +0000

    Add "PARK" angle preset
    I initially didn't include it as @jonemurray said he wasn't sure what it did, but after digging through the old program's source code a bit, it seems that it's just a preset angle like all the others (it seems to be ~90°). Let's include it for completeness.

--- finesse/config.py
+++ finesse/config.py
@@ -6,7 +6,7 @@ APP_NAME = "FINESSE"
 APP_AUTHOR = "Imperial College London"
 """The name of the app's author (used for program data path)."""
 
-ANGLE_PRESETS = ("zenith", "nadir", "hot_bb", "cold_bb", "home")
+ANGLE_PRESETS = ("zenith", "nadir", "hot_bb", "cold_bb", "home", "park")
 """Names of preset angles that the mirror can rotate to."""
 
 BAUDRATES = (4800, 9600, 19200, 38400, 57600, 115200)
