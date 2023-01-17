f41aa5ffa0e22c6bf0f3f77b3de47851424e9ea6
Alex Dewar <a.dewar@imperial.ac.uk>
2023-01-17 16:48:58 +0000

    Make OPUS HTTP requests on background thread

--- finesse/hardware/em27_opus.py
+++ finesse/hardware/em27_opus.py
@@ -10,6 +10,7 @@ from typing import Optional, cast
 import requests
 from bs4 import BeautifulSoup
 from pubsub import pub
+from PySide6.QtCore import QObject, QThread, Signal, Slot
 
 from ..config import OPUS_IP
 
@@ -17,53 +18,98 @@ STATUS_FILENAME = "stat.htm"
 COMMAND_FILENAME = "cmd.htm"
 
 
-def make_request(filename: str, topic: str):
-    """Make a request to the OPUS program.
+class OPUSRequester(QObject):
+    """Object which makes HTTP requests.
 
-    Args:
-        filename: The final part of the path on the HTTP server
-        topic: The topic on which to publish the response
+    The reason this has to be done as a class is because we need to inherit from QObject
+    so that this task can be assigned to a background thread.
     """
-    url = f"http://{OPUS_IP}/opusrs/{filename}"
-    response = requests.get(url)
-    assert response.status_code == 200
 
-    status: Optional[int] = None
-    text = ""
-    errcode: Optional[int] = None
-    errtext = ""
-    soup = BeautifulSoup(response.content, "html.parser")
-    for td in soup.find_all("td"):
-        if "id" not in td.attrs:
-            continue
+    request_complete = Signal(requests.Request, str)
 
-        id = td.attrs["id"]
-        data = td.contents[0]
-        if id == "STATUS":
-            status = int(data)
-        elif id == "TEXT":
-            text = data
-        elif id == "ERRCODE":
-            errcode = int(data)
-        elif id == "ERRTEXT":
-            errtext = data
+    @Slot()
+    def make_request(self, filename: str, topic: str):
+        """Make a request to the OPUS program.
 
-    if status is None or not text:
-        raise ValueError("Required tags not found")
-    error = None if errcode is None else (errcode, errtext)
-
-    pub.sendMessage(topic, url=url, status=cast(int, status), text=text, error=error)
+        Args:
+            filename: The final part of the path on the HTTP server
+            topic: The topic on which to publish the response
+        """
+        url = f"http://{OPUS_IP}/opusrs/{filename}"
+        response = requests.get(url)
+        self.request_complete.emit(response, topic)
 
 
-def request_status() -> None:
-    """Request an update on the device's status."""
-    make_request(STATUS_FILENAME, "opus.status.response")
+class OPUSInterface(QObject):
+    """Interface for communicating with the OPUS program.
 
+    HTTP requests are handled on a background thread.
+    """
 
-def request_command(command: str) -> None:
-    """Request that OPUS run the specified command."""
-    make_request(f"{COMMAND_FILENAME}?opusrs{command}", "opus.command.response")
+    submit_request = Signal(str, str)
 
+    def __init__(self) -> None:
+        """Create a new OPUSInterface."""
+        super().__init__()
 
-pub.subscribe(request_status, "opus.status.request")
-pub.subscribe(request_command, "opus.command.request")
+        # Make a new QThread to run HTTP requests in background
+        self.request_thread = QThread(self)
+        self.requester = OPUSRequester()
+        self.requester.moveToThread(self.request_thread)
+        self.requester.request_complete.connect(self._parse_response)
+
+        # Set up a signal for communicating between threads
+        self.submit_request.connect(self.requester.make_request)
+
+        # Subscribe to requests coming from the GUI
+        pub.subscribe(self.request_status, "opus.status.request")
+        pub.subscribe(self.request_command, "opus.command.request")
+
+        # Start processing requests
+        self.request_thread.start()
+
+    def __del__(self) -> None:
+        """Stop the background request thread."""
+        self.request_thread.quit()
+        self.request_thread.wait()
+
+    def _parse_response(self, response: requests.Response, topic: str) -> None:
+        assert response.status_code == 200
+
+        status: Optional[int] = None
+        text = ""
+        errcode: Optional[int] = None
+        errtext = ""
+        soup = BeautifulSoup(response.content, "html.parser")
+        for td in soup.find_all("td"):
+            if "id" not in td.attrs:
+                continue
+
+            id = td.attrs["id"]
+            data = td.contents[0]
+            if id == "STATUS":
+                status = int(data)
+            elif id == "TEXT":
+                text = data
+            elif id == "ERRCODE":
+                errcode = int(data)
+            elif id == "ERRTEXT":
+                errtext = data
+
+        if status is None or not text:
+            raise ValueError("Required tags not found")
+        error = None if errcode is None else (errcode, errtext)
+
+        pub.sendMessage(
+            topic, url=response.url, status=cast(int, status), text=text, error=error
+        )
+
+    def request_status(self) -> None:
+        """Request an update on the device's status."""
+        self.submit_request.emit(STATUS_FILENAME, "opus.status.response")
+
+    def request_command(self, command: str) -> None:
+        """Request that OPUS run the specified command."""
+        self.submit_request.emit(
+            f"{COMMAND_FILENAME}?opusrs{command}", "opus.command.response"
+        )
