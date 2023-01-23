b6020b7af7c47a73098637e81280120b48db903b
Alex Dewar <a.dewar@imperial.ac.uk>
2023-01-23 19:19:26 +0000

    Add error handling for HTTP requests + timeout

--- finesse/hardware/em27_opus.py
+++ finesse/hardware/em27_opus.py
@@ -5,6 +5,8 @@ Communication is based on a protocol using HTTP and HTML.
 The OPUS program must be running on the computer at OPUS_IP for the commands to work.
 Note that this is a separate machine from the EM27!
 """
+import logging
+import traceback
 from typing import Optional, cast
 
 import requests
@@ -18,14 +20,20 @@ STATUS_FILENAME = "stat.htm"
 COMMAND_FILENAME = "cmd.htm"
 
 
+class OPUSError(Exception):
+    """Indicates that an error occurred while communicating with the OPUS program."""
+
+
 class OPUSRequester(QThread):
     """Interface for making HTTP requests on a background thread."""
 
     request_complete = Signal(requests.Request, str)
+    request_error = Signal(BaseException)
 
-    def __init__(self) -> None:
+    def __init__(self, timeout: float) -> None:
         """Create a new OPUSRequester."""
         super().__init__()
+        self.timeout = timeout
         self.moveToThread(self)
 
     @Slot()
@@ -36,9 +44,12 @@ class OPUSRequester(QThread):
             filename: The final part of the path on the HTTP server
             topic: The topic on which to publish the response
         """
-        url = f"http://{OPUS_IP}/opusrs/{filename}"
-        response = requests.get(url)
-        self.request_complete.emit(response, topic)
+        try:
+            url = f"http://{OPUS_IP}/opusrs/{filename}"
+            response = requests.get(url, timeout=self.timeout)
+            self.request_complete.emit(response, topic)
+        except Exception as e:
+            self.request_error.emit(e)
 
 
 class OPUSInterface(QObject):
@@ -50,13 +61,18 @@ class OPUSInterface(QObject):
     submit_request = Signal(str, str)
     """Signal indicating that an HTTP request should be made."""
 
-    def __init__(self) -> None:
-        """Create a new OPUSInterface."""
+    def __init__(self, timeout: float = 3.0) -> None:
+        """Create a new OPUSInterface.
+
+        Args:
+            timeout: Amount of time before request times out (seconds)
+        """
         super().__init__()
 
-        self.requester = OPUSRequester()
+        self.requester = OPUSRequester(timeout)
         """For running HTTP requests in the background."""
         self.requester.request_complete.connect(self._parse_response)
+        self.requester.request_error.connect(self._error_occurred)
 
         # Set up a signal for communicating between threads
         self.submit_request.connect(self.requester.make_request)
@@ -74,35 +90,51 @@ class OPUSInterface(QObject):
         self.requester.wait()
 
     def _parse_response(self, response: requests.Response, topic: str) -> None:
-        assert response.status_code == 200
+        try:
+            if response.status_code != 200:
+                raise OPUSError(f"HTTP status code {response.status_code}")
 
-        status: Optional[int] = None
-        text = ""
-        errcode: Optional[int] = None
-        errtext = ""
-        soup = BeautifulSoup(response.content, "html.parser")
-        for td in soup.find_all("td"):
-            if "id" not in td.attrs:
-                continue
+            status: Optional[int] = None
+            text = ""
+            errcode: Optional[int] = None
+            errtext = ""
+            soup = BeautifulSoup(response.content, "html.parser")
+            for td in soup.find_all("td"):
+                if "id" not in td.attrs:
+                    continue
 
-            id = td.attrs["id"]
-            data = td.contents[0]
-            if id == "STATUS":
-                status = int(data)
-            elif id == "TEXT":
-                text = data
-            elif id == "ERRCODE":
-                errcode = int(data)
-            elif id == "ERRTEXT":
-                errtext = data
+                id = td.attrs["id"]
+                data = td.contents[0]
+                if id == "STATUS":
+                    status = int(data)
+                elif id == "TEXT":
+                    text = data
+                elif id == "ERRCODE":
+                    errcode = int(data)
+                elif id == "ERRTEXT":
+                    errtext = data
+                else:
+                    logging.warning(f"Received unknown ID: {id}")
 
-        if status is None or not text:
-            raise ValueError("Required tags not found")
-        error = None if errcode is None else (errcode, errtext)
+            if status is None or not text:
+                raise OPUSError("Required tags not found")
+            error = None if errcode is None else (errcode, errtext)
+        except Exception as e:
+            self._error_occurred(e)
+            return
 
         pub.sendMessage(
             topic, url=response.url, status=cast(int, status), text=text, error=error
         )
+
+    def _error_occurred(self, exception: BaseException) -> None:
+        traceback_str = "".join(traceback.format_tb(exception.__traceback__))
+
+        # Write details including stack trace to program log
+        logging.error(f"Error during OPUS request: {traceback_str}")
+
+        # Notify listeners
+        pub.sendMessage("opus.error", message=str(exception))
 
     def request_status(self) -> None:
         """Request an update on the device's status."""
