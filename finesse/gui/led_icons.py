4799763e123b54bc94bebb61825b8c10a20a3cce
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2023-01-25 14:00:01 +0000

    Added led_icons module
    Created a new led_icons module to store icon image data and abstract icon behaviour. Since poll icons are used across different components of the GUI, it is preferable to load and store the images once rather than loading them separately for each GUI component that needs them. This will also provide benefits when future behaviour of icons is implemented.

---
+++ finesse/gui/led_icons.py
@@ -0,0 +1,83 @@
+"""Class for LED Icons."""
+from importlib import resources
+
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import QLabel
+
+img_files = resources.files("finesse.gui.images")
+poll_on_img_data = img_files.joinpath("poll_on.png").read_bytes()
+poll_off_img_data = img_files.joinpath("poll_off.png").read_bytes()
+alarm_on_img_data = img_files.joinpath("alarm_on.png").read_bytes()
+alarm_off_img_data = img_files.joinpath("alarm_off.png").read_bytes()
+
+poll_on_img = QImage.fromData(poll_on_img_data)
+poll_off_img = QImage.fromData(poll_off_img_data)
+alarm_on_img = QImage.fromData(alarm_on_img_data)
+alarm_off_img = QImage.fromData(alarm_off_img_data)
+
+
+class LEDIcon(QLabel):
+    """QLabel object to represent an LED with on/off status."""
+
+    def __init__(self, status: int) -> None:
+        """Creates the LED icon, sets its status and stores corresponding image data."""
+        super().__init__()
+        self._status = status
+        self._on_img = QImage()
+        self._off_img = QImage()
+
+    def _turn_on(self):
+        """Turns the LED on."""
+        self._status = 1
+        self.setPixmap(QPixmap(self._on_img))
+
+    def _turn_off(self):
+        """Turns the LED off."""
+        self._status = 0
+        self.setPixmap(QPixmap(self._off_img))
+
+    def _flash(self, duration: int):
+        """Turns the LED on for a specified duration.
+
+        Args:
+            duration (int): Number of milliseconds to keep LED lit for
+        """
+        self._turn_on()
+        # wait for <duration> ms
+        self._turn_off()
+
+
+class PollIcon(LEDIcon):
+    """QLabel object to represent an LED for polling server."""
+
+    def __init__(self, status: int) -> None:
+        """Creates the LED icon, sets its status and stores corresponding image data.
+
+        Args:
+            status (int): On/off status of LED. 0 = off, !0 = on
+        """
+        super().__init__(status=status)
+        self._on_img = poll_on_img
+        self._off_img = poll_off_img
+        if status:
+            self._turn_on()
+        else:
+            self._turn_off()
+
+
+class AlarmIcon(LEDIcon):
+    """QLabel object to represent an LED to indicate alarm status."""
+
+    def __init__(self, status: int) -> None:
+        """Creates the LED icon, sets its status and stores corresponding image data.
+
+        Args:
+            status (int): On/off status of LED. 0 = off, !0 = on
+        """
+        super().__init__(status=status)
+        self._on_img = alarm_on_img
+        self._off_img = alarm_off_img
+        if status:
+            self._turn_on()
+        else:
+            self._turn_off()
