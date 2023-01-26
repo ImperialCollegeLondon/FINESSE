43538ff44fbc8593ea4cb228b98e9c4967fd7df7
Alex Dewar <a.dewar@imperial.ac.uk>
2023-01-26 16:28:21 +0000

    Add initial support for async requests
    This functionality isn't yet being used anywhere.

--- tests/test_st10_controller.py
+++ tests/test_st10_controller.py
@@ -7,13 +7,31 @@ from unittest.mock import MagicMock, patch
 import pytest
 from serial import SerialException, SerialTimeoutException
 
-from finesse.hardware.st10_controller import ST10Controller, ST10ControllerError
+from finesse.hardware.st10_controller import (
+    ST10Controller,
+    ST10ControllerError,
+    _SerialReader,
+)
+
+
+class MockSerialReader(_SerialReader):
+    """A mock version of _SerialReader that runs on the main thread."""
+
+    def run(self) -> None:
+        """Override the run method to make the thread do nothing."""
+
+    def read_sync(self) -> str:
+        """Read synchronously (mocked)."""
+        self._process_read()
+        return super().read_sync()
 
 
 @pytest.fixture
+@patch("finesse.hardware.st10_controller._SerialReader", MockSerialReader)
 def dev() -> ST10Controller:
     """A fixture providing an ST10Controller with a patched Serial object."""
     serial = MagicMock()
+    serial.timeout = 5.0
 
     # These functions should all be called, but patch them for now as we test this
     # elsewhere
@@ -23,9 +41,11 @@ def dev() -> ST10Controller:
                 return ST10Controller(serial)
 
 
+@patch("finesse.hardware.st10_controller._SerialReader", MockSerialReader)
 def test_init() -> None:
     """Test __init__()."""
     serial = MagicMock()
+    serial.timeout = 5.0
 
     with patch.object(ST10Controller, "_check_device_id") as check_mock:
         with patch.object(ST10Controller, "stop_moving") as stop_mock:
@@ -39,8 +59,8 @@ def test_init() -> None:
 
 
 def read_mock(dev: ST10Controller, return_value: str):
-    """Patch the _read() method of dev."""
-    return patch.object(dev, "_read", return_value=return_value)
+    """Patch the _read_sync() method of dev."""
+    return patch.object(dev, "_read_sync", return_value=return_value)
 
 
 def test_write(dev: ST10Controller) -> None:
@@ -52,7 +72,7 @@ def test_write(dev: ST10Controller) -> None:
 def test_read_normal(dev: ST10Controller) -> None:
     """Test the _read() method with a valid message."""
     dev.serial.read_until.return_value = b"hello\r"
-    ret = dev._read()
+    ret = dev._read_sync()
     dev.serial.read_until.assert_called_with(b"\r")
     assert ret == "hello"
 
@@ -63,7 +83,7 @@ def test_read_error(dev: ST10Controller) -> None:
     dev.serial.read_until.side_effect = SerialException()
 
     with pytest.raises(SerialException):
-        dev._read()
+        dev._read_sync()
         dev.serial.read_until.assert_called_with(b"\r")
 
 
@@ -71,14 +91,14 @@ def test_read_timed_out(dev: ST10Controller) -> None:
     """Test the _read() method with a timed-out response."""
     dev.serial.read_until.return_value = b""
     with pytest.raises(SerialTimeoutException):
-        dev._read()
+        dev._read_sync()
 
 
 def test_read_non_ascii(dev: ST10Controller) -> None:
     """Test the _read() method with a non-ASCII response."""
     dev.serial.read_until.return_value = b"\xff\r"
     with pytest.raises(ST10ControllerError):
-        dev._read()
+        dev._read_sync()
 
 
 @pytest.mark.parametrize(
