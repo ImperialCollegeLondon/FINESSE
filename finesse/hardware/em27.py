ef9d7b79862701d097ab656db6c1d168d0f3f006
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2023-02-09 14:37:36 +0000

    Improvements to functionality, modularity and exception handling
    Split out extraction of the PSF27 sensor data from the method which reads the html from the server. Changed webpage decoding from utf-8 to ASCII. Added a timeout for opening the webpage. Added some preliminary exception handling and error checking. Added a dummy class for testing.

--- finesse/hardware/em27.py
+++ finesse/hardware/em27.py
@@ -1,8 +1,9 @@
 """This module provides an interface to the EM27 monitor.
 
-This is used to scrape the data table off the server.
+This is used to scrape the PSF27 sensor data table off the server.
 """
 from typing import Dict, Tuple
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
 
@@ -13,48 +14,103 @@ class EM27:
         """Create a new EM27 monitor.
 
         Args:
-            url: "Web address of the automation units diagnostics page.
+            url: Web address of the automation units diagnostics page.
         """
-        # "file:///home/dc2917/Downloads/Automation units Diagnostics.html"
         self._url = url
+        self._html = b""
         self._data_table: Dict[str, Tuple[float, str]] = {}
 
-    def open(self):
-        """Connect to the webpage."""
-        self._page = urlopen(self._url)
+    def open(self, timeout: int = 2) -> int:
+        """Connect to the webpage.
+
+        Args:
+            timeout: Number of seconds to wait for response.
+
+        Returns:
+            error: 0 = successful request, 1 = error
+        """
+        error = 1
+        try:
+            self._page = urlopen(self._url, timeout=timeout)
+            error = 0
+        except HTTPError as exception:
+            print(exception.status, exception.reason)
+        except URLError as exception:
+            print(exception.reason)
+        except TimeoutError:
+            print("Request timed out")
+
+        return error
 
     def close(self):
         """Disconnect from the webpage."""
         self._page.close()
 
-    def read(self):
-        """Read the webpage, search for the table and store the data."""
-        html_bytes = self._page.read()
-        html = html_bytes.decode("utf-8")
+    def read(self) -> int:
+        """Read the webpage and store in EM27 object.
+
+        Returns:
+            error: 0 = successful read, 1 = error
+        """
+        try:
+            self._html = self._page.read()
+            error = 0
+        except ValueError as exception:
+            print(exception)
+            print("Open page first")
+            error = 1
+        return error
+
+    def get_psf27sensor_data(self) -> int:
+        """Search for the PSF27Sensor table and store the data.
+
+        Returns:
+            error: 0 = successful request, 1 = error
+        """
+        error = 0
+        html_ascii = self._html.decode("ascii")
         table_header = (
             "<TR><TH>No</TH><TH>Name</TH><TH>Description</TH>"
             + "<TH>Status</TH><TH>Value</TH><TH>Meas. Unit</TH></TR>\n"
-            # "<tbody><tr><th>No</th><th>Name</th><th>Description</th>"
-            # + "<th>Status</th><th>Value</th><th>Meas. Unit</th></tr>\n"
         )
-        table_start = html.find(table_header)
+        table_start = html_ascii.find(table_header)
         if table_start == -1:
+            error = 1
             print("Error: table not located")
-            print(html)
-            return
-        table_end = table_start + html[table_start:].find("</TABLE>")
-        table = html[table_start:table_end].splitlines()
-        for row in range(1, len(table) - 1):
-            self._data_table[table[row].split("<TD>")[2].rstrip("</TD>")] = (
-                float(table[row].split("<TD>")[5].strip("</TD>")),
-                table[row].split("<TD>")[6].rstrip("</TD></TR"),
-            )
+            print(html_ascii)
+        else:
+            table_end = table_start + html_ascii[table_start:].find("</TABLE>")
+            table = html_ascii[table_start:table_end].splitlines()
+            for row in range(1, len(table) - 1):
+                self._data_table[table[row].split("<TD>")[2].rstrip("</TD>")] = (
+                    float(table[row].split("<TD>")[5].strip("</TD>")),
+                    table[row].split("<TD>")[6].rstrip("</TD></TR"),
+                )
+        return error
+
+
+class DummyEM27(EM27):
+    """An interface for EM27 monitor testing."""
+
+    def __init__(self) -> None:
+        """Create a new monitor for a dummy EM27."""
+        super().__init__("file:///home/dc2917/Downloads/diag_autom.htm")
 
 
 if __name__ == "__main__":
 
-    dev = EM27()
-    dev.open()
-    dev.read()
+    dev = DummyEM27()
+    error = dev.open()
+    print(error)
+    error = dev.read()
+    print(error)
+    error = dev.get_psf27sensor_data()
+    print(error)
     print(dev._data_table)
     dev.close()
+
+    # tests:
+    # 1) open correct webpage, check response
+    # 1a)
+    # 1b)
+    # 2) attempt to open incorrect webpage
