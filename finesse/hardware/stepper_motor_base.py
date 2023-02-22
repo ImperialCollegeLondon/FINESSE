4a8536794715163a0573c229cff6c2644eef379d
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2023-02-22 13:37:50 +0000

    Integrated LEDIcons, minor changes to layout.
    Merged in LEDIcon module, so poll icon on EM27 monitor now uses this. Set the size policy of this LEDIcon instance to that of the text boxes on this panel for equal spacing and sizing of all rows. Set the left-hand side of the main window to follow a two-column grid layout.

---
+++ finesse/hardware/stepper_motor_base.py
@@ -0,0 +1,94 @@
+"""Provides the base class for stepper motor implementations."""
+from abc import ABC, abstractmethod
+from typing import Optional, Union
+
+from pubsub import pub
+
+from ..config import ANGLE_PRESETS
+
+
+class StepperMotorBase(ABC):
+    """A base class for stepper motor implementations."""
+
+    def __init__(self) -> None:
+        """Create a new StepperMotorBase.
+
+        Subscribe to stepper.move messages.
+        """
+        pub.subscribe(self.move_to, "stepper.move.begin")
+        pub.subscribe(self.stop_moving, "stepper.stop")
+        pub.subscribe(self.notify_on_stopped, "stepper.notify_on_stopped")
+
+    @staticmethod
+    def preset_angle(name: str) -> float:
+        """Get the angle for one of the preset positions.
+
+        Args:
+            name: Name of preset angle
+
+        Returns:
+            The angle in degrees
+        """
+        try:
+            return ANGLE_PRESETS[name]
+        except KeyError as e:
+            raise ValueError(f"{name} is not a valid preset") from e
+
+    @property
+    @abstractmethod
+    def steps_per_rotation(self) -> int:
+        """The number of steps that correspond to a full rotation."""
+
+    @property
+    @abstractmethod
+    def step(self) -> int:
+        """The current state of the device's step counter."""
+
+    @step.setter
+    @abstractmethod
+    def step(self, step: int) -> None:
+        """Move the stepper motor to the specified absolute position.
+
+        Args:
+            step: Which step position to move to
+        """
+
+    @abstractmethod
+    def stop_moving(self) -> None:
+        """Immediately stop moving the motor."""
+
+    @abstractmethod
+    def wait_until_stopped(self, timeout: Optional[float] = None) -> None:
+        """Wait until the motor has stopped moving.
+
+        Args:
+            timeout: Time to wait for motor to finish moving (None == forever)
+        """
+
+    @abstractmethod
+    def notify_on_stopped(self) -> None:
+        """Wait until the motor has stopped moving and send a message when done.
+
+        The message is stepper.move.end.
+        """
+
+    @property
+    def angle(self) -> float:
+        """The current angle of the motor in degrees."""
+        return self.step * 360.0 / self.steps_per_rotation
+
+    def move_to(self, target: Union[float, str]) -> None:
+        """Move the motor to a specified rotation and send message when complete.
+
+        Sends a stepper.move.end message when finished.
+
+        Args:
+            target: The target angle (in degrees) or the name of a preset
+        """
+        if isinstance(target, str):
+            target = self.preset_angle(target)
+
+        if target < 0.0 or target > 270.0:
+            raise ValueError("Angle must be between 0° and 270°")
+
+        self.step = round(self.steps_per_rotation * target / 360.0)
