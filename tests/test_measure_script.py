38b065f2e95a482e5b59cc59190a8b2f719f7b7a
Alex Dewar <a.dewar@imperial.ac.uk>
2023-02-28 12:14:08 +0000

    Add another test

--- tests/test_measure_script.py
+++ tests/test_measure_script.py
@@ -1,13 +1,13 @@
 """Tests for the measure script code."""
 from contextlib import nullcontext as does_not_raise
 from itertools import chain
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 import pytest
 import yaml
 
 from finesse.config import ANGLE_PRESETS
-from finesse.gui.measure_script.script import ParseError, parse_script
+from finesse.gui.measure_script.script import Measurement, ParseError, parse_script
 
 
 def is_valid_angle(angle: Any) -> bool:
@@ -67,3 +67,10 @@ def test_parse_script(data: Dict[str, Any], raises: Any) -> None:
     """
     with raises:
         parse_script(yaml.safe_dump(data))
+
+
+@pytest.mark.parametrize("angle", ("nadir", 90.0))
+def test_measurement_to_dict(angle: Union[str, float]) -> None:
+    """Check Measurement's to_dict() method."""
+    d = Measurement(angle, 5)
+    assert d.to_dict() == {"angle": angle, "measurements": 5}
