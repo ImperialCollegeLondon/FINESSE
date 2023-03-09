599ee5d5f7ac1684c959fc14d41e9283f66dcf83
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2023-03-09 10:24:09 +0000

    Removed EM27Property class - now defined in webscraper module

--- finesse/gui/interferometer_monitor.py
+++ finesse/gui/interferometer_monitor.py
@@ -1,5 +1,4 @@
 """Panel and widgets related to monitoring the interferometer."""
-from dataclasses import dataclass
 from decimal import Decimal
 from typing import Dict
 
@@ -15,31 +14,8 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+from ..hardware.em27_diag_autom import EM27Property
 from .led_icons import LEDIcon
-
-
-@dataclass
-class EM27Property:
-    """Class for representing EM27 monitored properties.
-
-    Args:
-        name: name of the physical quantity
-        value: value of the physical quantity
-        unit: unit in which the value is presented
-    """
-
-    name: str
-    value: Decimal
-    unit: str
-
-    def val_str(self) -> str:
-        """For printing a property's value and unit in required format.
-
-        Returns:
-            str: The value and unit of a property in the format consistent with
-                 the previous FINESSE GUI.
-        """
-        return f"{self.value:.6f} {self.unit}"
 
 
 def get_vals_from_server() -> list[EM27Property]:
