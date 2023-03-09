6592380e99884b4d7537e97efa1c4197f835b75e
Alex Dewar <a.dewar@imperial.ac.uk>
2023-03-09 15:34:17 +0000

    Add a class for producing random noise
    This will be used for dummy devices.

---
+++ tests/test_noise_producer.py
@@ -0,0 +1,46 @@
+"""Tests for the NoiseProducer class."""
+from decimal import Decimal
+from typing import Optional
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+
+from finesse.hardware.noise_producer import NoiseProducer
+
+
+@pytest.mark.parametrize("seed", (None, 0, 42))
+@patch("finesse.hardware.noise_producer.np.random.default_rng")
+def test_init(rng_mock: Mock, seed: Optional[int]) -> None:
+    """Test providing different seeds to NoiseProducer."""
+    NoiseProducer(seed=seed)
+    rng_mock.assert_called_once_with(seed)
+
+
+@patch("finesse.hardware.noise_producer.np.random.default_rng")
+def test_init_defaults(rng_mock: Mock) -> None:
+    """Test the default values for NoiseProducer's constructor."""
+    mock2 = MagicMock()
+    rng_mock.return_value = mock2
+    noise = NoiseProducer()
+    assert noise.mean == 0.0
+    assert noise.standard_deviation == 1.0
+    assert noise.type == float
+    rng_mock.assert_called_once_with(42)
+    assert noise.rng is mock2
+
+
+@pytest.mark.parametrize(
+    "mean,standard_deviation,type", zip(range(3), range(3), (float, int, Decimal))
+)
+def test_call(mean: int, standard_deviation: int, type: type) -> None:
+    """Test calling NoiseProducers."""
+    noise = NoiseProducer(float(mean), float(standard_deviation), type)
+    with patch.object(noise, "rng") as rng_mock:
+        rng_mock.normal.return_value = 0.0
+        value = noise()
+
+        # Check the RNG is called with the correct mean and SD
+        rng_mock.normal.assert_called_once_with(float(mean), float(standard_deviation))
+
+        # Check that returned type is correct
+        assert isinstance(value, type)
