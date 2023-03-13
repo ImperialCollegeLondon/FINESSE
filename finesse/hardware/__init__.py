ba7b40b82382fb66076b197b0fb4b1174dfdb640
Alex Dewar <a.dewar@imperial.ac.uk>
2023-03-13 19:31:51 +0000

    Add SerialManagers for the temperature controller devices

--- finesse/hardware/__init__.py
+++ finesse/hardware/__init__.py
@@ -9,6 +9,7 @@ else:
     from .opus.em27 import OPUSInterface  # type: ignore
 
 from .stepper_motor import create_stepper_motor_serial_manager
+from .temperature import create_temperature_controller_serial_managers
 
 opus: OPUSInterface
 
@@ -28,3 +29,4 @@ pub.subscribe(_init_hardware, "window.opened")
 pub.subscribe(_stop_hardware, "window.closed")
 
 create_stepper_motor_serial_manager()
+create_temperature_controller_serial_managers()
--- finesse/hardware/temperature/__init__.py
+++ finesse/hardware/temperature/__init__.py
@@ -1,1 +1,25 @@
 """This module contains interfaces for temperature-related hardware."""
+from functools import partial
+
+from ...config import TEMPERATURE_CONTROLLER_TOPIC
+from ..serial_manager import SerialManager
+from .dummy_temperature_controller import DummyTemperatureController
+from .tc4820 import TC4820
+
+_serial_manager_hot_bb: SerialManager
+_serial_manager_cold_bb: SerialManager
+
+
+def _create_serial_manager(name: str) -> SerialManager:
+    return SerialManager(
+        f"{TEMPERATURE_CONTROLLER_TOPIC}.{name}",
+        partial(TC4820, name),
+        partial(DummyTemperatureController, name),
+    )
+
+
+def create_temperature_controller_serial_managers() -> None:
+    """Create SerialManagers for the cold and hot black body temperature controllers."""
+    global _serial_manager_cold_bb, _serial_manager_hot_bb
+    _serial_manager_hot_bb = _create_serial_manager("hot_bb")
+    _serial_manager_cold_bb = _create_serial_manager("cold_bb")
