71c8c3ce0c2b362ca6df2c848eefd62dacbe660d
Alex Dewar <a.dewar@imperial.ac.uk>
2023-03-20 14:38:24 +0000

    Put dummy/real device creation logic into separate factory functions
    This makes SerialManager a bit simpler internally and abstracts things properly.

--- finesse/hardware/stepper_motor/__init__.py
+++ finesse/hardware/stepper_motor/__init__.py
@@ -2,7 +2,7 @@
 from functools import partial
 
 from ...config import STEPPER_MOTOR_TOPIC
-from ..serial_manager import SerialManager
+from ..serial_manager import SerialManager, make_device_factory
 from .dummy import DummyStepperMotor
 from .st10_controller import ST10Controller
 
@@ -13,5 +13,6 @@ def create_stepper_motor_serial_manager() -> None:
     """Create a SerialManager for the stepper motor device."""
     global _serial_manager
     _serial_manager = SerialManager(
-        STEPPER_MOTOR_TOPIC, ST10Controller, partial(DummyStepperMotor, 3600)
+        STEPPER_MOTOR_TOPIC,
+        make_device_factory(ST10Controller, partial(DummyStepperMotor, 3600)),
     )
--- tests/hardware/test_serial_manager.py
+++ tests/hardware/test_serial_manager.py
@@ -6,7 +6,7 @@ from pubsub import pub
 from serial import SerialException
 
 from finesse.config import DUMMY_DEVICE_PORT
-from finesse.hardware.serial_manager import SerialManager
+from finesse.hardware.serial_manager import SerialManager, make_device_factory
 
 
 @pytest.fixture
@@ -17,24 +17,43 @@ def unsubscribe_mock(monkeypatch) -> MagicMock:
     return mock
 
 
-def test_init(subscribe_mock: MagicMock) -> None:
-    """Test SerialManager's constructor."""
-    manager = SerialManager("my_device", MagicMock(), MagicMock())
-    subscribe_mock.assert_any_call(manager._open, "serial.my_device.open")
-
-
 @patch("finesse.hardware.serial_manager.Serial")
-def test_open_real_success(
-    serial_mock: Mock, sendmsg_mock: MagicMock, subscribe_mock: MagicMock
-) -> None:
-    """Test the _open() method with a real serial device that opens successfully."""
+def test_make_device_factory_real(serial_mock: Mock) -> None:
+    """Test the make_device_factory() function when constructing a real device."""
     serial = MagicMock()
     serial_mock.return_value = serial
 
-    device_ctor = MagicMock()
-    manager = SerialManager("my_device", device_ctor, MagicMock())
+    device_factory = MagicMock()
+    device_factory.return_value = "MAGIC"
+    create_device = make_device_factory(device_factory, MagicMock())
+    assert create_device("COM1", 1234) == "MAGIC"
+    serial_mock.assert_called_once_with("COM1", 1234)
+    device_factory.assert_called_once_with(serial)
+
+
+def test_make_device_factory_dummy() -> None:
+    """Test the make_device_factory() function when constructing a dummy device."""
+    dummy_device_factory = MagicMock()
+    dummy_device_factory.return_value = "MAGIC"
+    create_device = make_device_factory(MagicMock(), dummy_device_factory)
+    assert create_device(DUMMY_DEVICE_PORT, 1234) == "MAGIC"
+    dummy_device_factory.assert_called_once_with()
+
+
+def test_init(subscribe_mock: MagicMock) -> None:
+    """Test SerialManager's constructor."""
+    device_factory = MagicMock()
+    manager = SerialManager("my_device", device_factory)
+    assert manager.device_factory == device_factory
+    subscribe_mock.assert_any_call(manager._open, "serial.my_device.open")
+
+
+def test_open_real_success(sendmsg_mock: MagicMock, subscribe_mock: MagicMock) -> None:
+    """Test the _open() method with a device that opens successfully."""
+    device_factory = MagicMock()
+    manager = SerialManager("my_device", device_factory)
     manager._open("COM1", 1234)
-    device_ctor.assert_called_once_with(serial)
+    device_factory.assert_called_once_with("COM1", 1234)
     subscribe_mock.assert_any_call(manager._close, "serial.my_device.close")
     subscribe_mock.assert_any_call(
         manager._send_close_message, "serial.my_device.error"
@@ -42,41 +61,28 @@ def test_open_real_success(
     sendmsg_mock.assert_any_call("serial.my_device.opened")
 
 
-@patch("finesse.hardware.serial_manager.Serial")
-def test_open_real_fail(serial_mock: Mock, sendmsg_mock: MagicMock) -> None:
-    """Test the _open() method with a real serial device that fails to open."""
+def test_open_real_fail(sendmsg_mock: MagicMock) -> None:
+    """Test the _open() method with a device that fails to open."""
+    device_factory = MagicMock()
     error = SerialException()
-    serial_mock.side_effect = error
+    device_factory.side_effect = error
 
-    manager = SerialManager("my_device", MagicMock(), MagicMock())
+    manager = SerialManager("my_device", device_factory)
     manager._open("COM1", 1234)
     sendmsg_mock.assert_called_once_with("serial.my_device.error", error=error)
 
 
-def test_open_dummy(sendmsg_mock: MagicMock, subscribe_mock: MagicMock) -> None:
-    """Test the _open() method with a dummy serial device."""
-    dummy_device_ctor = MagicMock()
-    manager = SerialManager("my_device", MagicMock(), dummy_device_ctor)
-    manager._open(DUMMY_DEVICE_PORT, 1234)
-    dummy_device_ctor.assert_called_once_with()
-    subscribe_mock.assert_any_call(manager._close, "serial.my_device.close")
-    subscribe_mock.assert_any_call(
-        manager._send_close_message, "serial.my_device.error"
-    )
-    sendmsg_mock.assert_any_call("serial.my_device.opened")
-
-
 def test_close(unsubscribe_mock: MagicMock) -> None:
     """Test the _close() method."""
-    dummy_device_ctor = MagicMock()
-    device_mock = MagicMock()
-    dummy_device_ctor.return_value = device_mock
-    manager = SerialManager("my_device", MagicMock(), dummy_device_ctor)
+    device_factory = MagicMock()
+    device = MagicMock()
+    device_factory.return_value = device
+    manager = SerialManager("my_device", device_factory)
     manager._open(DUMMY_DEVICE_PORT, 1234)
     manager._close()
 
     # Check the device was closed
-    device_mock.close.assert_called_once_with()
+    device.close.assert_called_once_with()
 
     # Check manager has unsubscribed from future close events
     unsubscribe_mock.assert_any_call(manager._close, "serial.my_device.close")
