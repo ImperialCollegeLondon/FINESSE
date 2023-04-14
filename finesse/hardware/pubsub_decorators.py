6890308ba617845a07cee7c01b6718283fd261c1
Alex Dewar <a.dewar@imperial.ac.uk>
2023-04-14 18:01:10 +0100

    Print traceback when pubsub errors are sent

--- finesse/hardware/pubsub_decorators.py
+++ finesse/hardware/pubsub_decorators.py
@@ -1,8 +1,21 @@
 """Provides a decorator for catching and forwarding errors via pubsub."""
+import logging
+import traceback
 from collections.abc import Callable
 
 from decorator import decorator
 from pubsub import pub
+
+
+def _error_occurred(error_topic: str, error: BaseException) -> None:
+    """Signal that an error occurred."""
+    traceback_str = "".join(traceback.format_tb(error.__traceback__))
+
+    # Write details including stack trace to program log
+    logging.error(f"Caught error ({error_topic}): {traceback_str}")
+
+    # Notify listeners
+    pub.sendMessage(error_topic, error=error)
 
 
 def pubsub_errors(error_topic: str) -> Callable:
@@ -16,7 +29,7 @@ def pubsub_errors(error_topic: str) -> Callable:
         try:
             return func(*args, **kwargs)
         except Exception as error:
-            pub.sendMessage(error_topic, error=error)
+            _error_occurred(error_topic, error)
 
     return decorator(wrapped)
 
@@ -39,7 +52,7 @@ def pubsub_broadcast(
         try:
             result = func(*args, **kwargs)
         except Exception as error:
-            pub.sendMessage(error_topic, error=error)
+            _error_occurred(error_topic, error)
         else:
             # Convert result to a tuple of the right size
             if result is None:
