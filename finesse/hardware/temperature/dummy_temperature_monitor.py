8e5f1105aff55292743a7408257aa8de0ae66b56
Alex Dewar <a.dewar@imperial.ac.uk>
2023-04-14 10:08:29 +0100

    Refactor temperature monitors
    Make a get_temperatures() method be the abstract method that child classes override rather than send_temperatures(), whose implementation can live in the parent class.

--- finesse/hardware/temperature/dummy_temperature_monitor.py
+++ finesse/hardware/temperature/dummy_temperature_monitor.py
@@ -22,15 +22,8 @@ class DummyTemperatureMonitor(TemperatureMonitorBase):
         pub.sendMessage("temperature_monitor.close")
         logging.info("Closed connection to dummy temperature monitor")
 
-    def send_temperatures(self) -> None:
-        """Publish fluctuating temperatures."""
+    def get_temperatures(self) -> list[Decimal]:
+        """Get current temperatures."""
         _BASE_TEMPS = (19, 17, 26, 22, 24, 68, 69, 24)
         noise = self._noise_producer()
-        temperatures = [Decimal(noise + temp) for temp in _BASE_TEMPS]
-        time_now = datetime.now().timestamp()
-
-        pub.sendMessage(
-            "temperature_monitor.data.response",
-            temperatures=temperatures,
-            time=time_now,
-        )
+        return [Decimal(noise + temp) for temp in _BASE_TEMPS]
