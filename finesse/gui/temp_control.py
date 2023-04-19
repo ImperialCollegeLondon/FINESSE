f62d6d256b1316ccc24606fcba8cc2d00953b088
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2023-04-19 13:41:07 +0100

    Disabled cold blackbody's UPDATE button

--- finesse/gui/temp_control.py
+++ finesse/gui/temp_control.py
@@ -386,9 +386,11 @@ class TC4820Controls(SerialDevicePanel):
 
     def _begin_polling(self) -> None:
         """Initiate polling the TC4820 device."""
-        # SerialDevicePanel.set_controls_enabled will enable this, but
-        # want it to begin disabled
+        # SerialDevicePanel.set_controls_enabled() will enable these, but
+        # we want them to begin disabled
         self._set_sbox.setEnabled(False)
+        if self._name.count("cold"):
+            self._update_pbtn.setEnabled(False)
         self._poll_light.timer.start(self._poll_interval)
 
     def _end_polling(self) -> None:
