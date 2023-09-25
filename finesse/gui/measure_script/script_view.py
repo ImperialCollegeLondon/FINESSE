ImperialCollegeLondon/FINESSE

commit 91e2843c2bec17cda1cf236b8505991170d30e7b

Add timestamps to CSV filenames
Now the user chooses the destination folder and a filename prefix
instead and the timestamp is appended to the end (with the extension).

Closes #282. Closes #249.

finesse/gui/measure_script/script_view.py
@@ -9,7 +9,7 @@ from ...config import DEFAULT_SCRIPT_PATH, STEPPER_MOTOR_TOPIC
 from ...em27_status import EM27Status
 from ...event_counter import EventCounter
 from ...settings import settings
-from ..path_widget import OpenPathWidget
+from ..path_widget import OpenDirectoryWidget
 from .script import Script, ScriptRunner
 from .script_edit_dialog import ScriptEditDialog
 from .script_run_dialog import ScriptRunDialog
@@ -33,8 +33,8 @@ class ScriptControl(QGroupBox):
         edit_btn = QPushButton("Edit script")
         edit_btn.clicked.connect(self._edit_btn_clicked)
 
-        self.script_path = OpenPathWidget(
-            initial_file_path=_get_previous_script_path(),
+        self.script_path = OpenDirectoryWidget(
+            initial_dir_path=_get_previous_script_path(),
             extension="yaml",
             parent=self,
             caption="Choose measure script to load",

finesse/gui/data_file_view.py
@@ -1,17 +1,20 @@
 """Provides a panel which lets the user start and stop recording of data files."""
+from datetime import datetime
 from pathlib import Path
 
 from pubsub import pub
 from PySide6.QtWidgets import (
     QGroupBox,
     QHBoxLayout,
+    QLabel,
+    QLineEdit,
     QMessageBox,
     QPushButton,
     QSizePolicy,
 )
 
 from ..config import DEFAULT_DATA_FILE_PATH
-from .path_widget import SavePathWidget
+from .path_widget import OpenDirectoryWidget
 
 
 class DataFileControl(QGroupBox):
@@ -23,14 +26,18 @@ class DataFileControl(QGroupBox):
 
         layout = QHBoxLayout()
 
-        self.save_path_widget = SavePathWidget(
-            extension="csv",
+        self.open_dir_widget = OpenDirectoryWidget(
             parent=self,
             caption="Choose destination for data file",
             dir=str(DEFAULT_DATA_FILE_PATH),
         )
         """Lets the user choose the destination for data files."""
-        layout.addWidget(self.save_path_widget)
+        layout.addWidget(QLabel("Destination directory:"))
+        layout.addWidget(self.open_dir_widget)
+
+        self.filename_prefix_widget = QLineEdit()
+        layout.addWidget(QLabel("Filename prefix:"))
+        layout.addWidget(self.filename_prefix_widget)
 
         self.record_btn = QPushButton("Start recording")
         """Toggles recording state."""
@@ -52,34 +59,50 @@ class DataFileControl(QGroupBox):
         pub.subscribe(self._show_error_message, "data_file.error")
 
     def _on_file_open(self, path: Path) -> None:
-        self.save_path_widget.setEnabled(False)
+        self.open_dir_widget.setEnabled(False)
+        self.filename_prefix_widget.setEnabled(False)
         self.record_btn.setText("Stop recording")
 
     def _on_file_close(self) -> None:
-        self.save_path_widget.setEnabled(True)
+        self.open_dir_widget.setEnabled(True)
+        self.filename_prefix_widget.setEnabled(True)
         self.record_btn.setText("Start recording")
 
-    def _user_confirms_overwrite(self, path: Path) -> bool:
-        """Confirm with the user whether to overwrite file via a dialog."""
-        response = QMessageBox.question(
-            self,
-            "Overwrite file?",
-            f"The file {path.name} already exists. Would you like to overwrite it?",
-            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
-        )
-        return response == QMessageBox.StandardButton.Yes
+    def _try_get_data_file_path(self) -> Path | None:
+        dest_dir = self.open_dir_widget.try_get_path()
+        if not dest_dir:
+            # User cancelled
+            return None
 
-    def _try_start_recording(self, path: Path) -> None:
-        """Start recording if path doesn't exist or user accepts overwriting it."""
-        if not path.exists() or self._user_confirms_overwrite(path):
-            pub.sendMessage("data_file.open", path=path)
+        filename_prefix = self.filename_prefix_widget.text()
+        if not filename_prefix:
+            # Check that the user has added a prefix
+            QMessageBox(
+                QMessageBox.Icon.Critical,
+                "No filename prefix specified",
+                "The filename prefix cannot be blank.",
+            ).exec()
+            return None
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        path = dest_dir / f"{filename_prefix}_{timestamp}.csv"
+        if path.exists():
+            # It's unlikely that the file will already exist, but let's make doubly sure
+            QMessageBox(
+                QMessageBox.Icon.Critical,
+                "File already exists",
+                "The destination file already exists.",
+            ).exec()
+            return None
+
+        return path
 
     def _toggle_recording(self) -> None:
         """Starts or stops recording as needed."""
         if self.record_btn.text() == "Stop recording":
             pub.sendMessage("data_file.close")
-        elif path := self.save_path_widget.try_get_path():
-            self._try_start_recording(path)
+        elif path := self._try_get_data_file_path():
+            pub.sendMessage("data_file.open", path=path)
 
     def _show_error_message(self, error: BaseException) -> None:
         """Show an error dialog."""

tests/gui/test_data_file_view.py
@@ -1,10 +1,8 @@
 """Tests for DataFileControl."""
-from itertools import product
 from pathlib import Path
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
-from PySide6.QtWidgets import QMessageBox
 
 from finesse.gui.data_file_view import DataFileControl
 
@@ -22,7 +20,8 @@ def test_init(subscribe_mock: MagicMock, qtbot) -> None:
     data_file = DataFileControl()
     assert data_file.record_btn.text() == "Start recording"
     assert data_file.record_btn.isEnabled()
-    assert data_file.save_path_widget.isEnabled()
+    assert data_file.open_dir_widget.isEnabled()
+    assert data_file.filename_prefix_widget.isEnabled()
 
     subscribe_mock.assert_any_call(data_file._on_file_open, "data_file.open")
     subscribe_mock.assert_any_call(data_file._on_file_close, "data_file.close")
@@ -33,24 +32,24 @@ def test_start_recording(
     data_file: DataFileControl, sendmsg_mock: MagicMock, qtbot
 ) -> None:
     """Test that recording starts correctly."""
-    with patch.object(data_file, "_try_start_recording") as try_start_mock:
+    with patch.object(data_file, "_try_get_data_file_path") as get_path_mock:
+        get_path_mock.return_value = FILE_PATH
         assert data_file.record_btn.text() == "Start recording"
-        data_file.save_path_widget.set_path(FILE_PATH)
         data_file.record_btn.click()
-        try_start_mock.assert_called_once_with(FILE_PATH)
+        sendmsg_mock.assert_called_once_with("data_file.open", path=FILE_PATH)
 
 
 def test_start_recording_path_dialog_cancelled(
     data_file: DataFileControl, sendmsg_mock: MagicMock, qtbot
 ) -> None:
     """Check that recording isn't started if the user closes the file dialog."""
-    assert data_file.save_path_widget.line_edit.text() == ""
-    with patch.object(data_file.save_path_widget, "try_get_path") as path_mock:
+    assert data_file.open_dir_widget.line_edit.text() == ""
+    with patch.object(data_file.open_dir_widget, "try_get_path") as path_mock:
         path_mock.return_value = None
         data_file.record_btn.click()
         path_mock.assert_called_once()
         assert data_file.record_btn.text() == "Start recording"
-        assert data_file.save_path_widget.isEnabled()
+        assert data_file.open_dir_widget.isEnabled()
         sendmsg_mock.assert_not_called()
 
 
@@ -69,7 +68,7 @@ def test_on_file_open(data_file: DataFileControl, qtbot) -> None:
     """Test the _on_file_open() method."""
     data_file._on_file_open(FILE_PATH)
     assert data_file.record_btn.text() == "Stop recording"
-    assert not data_file.save_path_widget.isEnabled()
+    assert not data_file.open_dir_widget.isEnabled()
 
 
 def test_on_file_close(data_file: DataFileControl, qtbot) -> None:
@@ -81,7 +80,7 @@ def test_on_file_close(data_file: DataFileControl, qtbot) -> None:
     data_file._on_file_close()
 
     assert data_file.record_btn.text() == "Start recording"
-    assert data_file.save_path_widget.isEnabled()
+    assert data_file.open_dir_widget.isEnabled()
 
 
 @patch("finesse.gui.data_file_view.QMessageBox")
@@ -100,50 +99,3 @@ def test_show_error_message(
         data_file,
     )
     msgbox.exec.assert_called_once_with()
-
-
-@pytest.mark.parametrize(
-    "response", (QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.No)
-)
-@patch("finesse.gui.data_file_view.QMessageBox")
-def test_user_confirms_overwrite(
-    msgbox_mock: Mock,
-    response: QMessageBox.StandardButton,
-    data_file: DataFileControl,
-    qtbot,
-) -> None:
-    """Test the _user_confirms_overwrite() method."""
-    msgbox_mock.StandardButton = QMessageBox.StandardButton
-    msgbox_mock.question.return_value = response
-    assert data_file._user_confirms_overwrite(FILE_PATH) == (
-        response == QMessageBox.StandardButton.Yes
-    )
-    msgbox_mock.question.assert_called_once_with(
-        data_file,
-        "Overwrite file?",
-        f"The file {FILE_PATH.name} already exists. Would you like to overwrite it?",
-        QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
-    )
-
-
-@pytest.mark.parametrize("exists,user_confirms", product((True, False), repeat=2))
-def test_try_start_recording(
-    exists: bool,
-    user_confirms: bool,
-    sendmsg_mock: MagicMock,
-    data_file: DataFileControl,
-    qtbot,
-) -> None:
-    """Test the _try_start_recording() method."""
-    with patch.object(
-        data_file, "_user_confirms_overwrite", MagicMock(return_value=user_confirms)
-    ) as confirm_mock:
-        confirm_mock.return_value = user_confirms
-        path = MagicMock()
-        path.exists.return_value = exists
-        data_file._try_start_recording(path)
-
-        if not exists or user_confirms:
-            sendmsg_mock.assert_called_once_with("data_file.open", path=path)
-        else:
-            sendmsg_mock.assert_not_called()

commit 4c1d7c19c316459628c75238773f097b26a9394c

Fix broken script "Browse" button
Commit 91e2843 accidentally changed the type of ScriptControl.script_path to an OpenDirectoryWidget, whereas it needs to be an OpenPathWidget.

finesse/gui/measure_script/script_view.py
@@ -9,7 +9,7 @@ from ...config import DEFAULT_SCRIPT_PATH, STEPPER_MOTOR_TOPIC
 from ...em27_status import EM27Status
 from ...event_counter import EventCounter
 from ...settings import settings
-from ..path_widget import OpenDirectoryWidget
+from ..path_widget import OpenPathWidget
 from .script import Script, ScriptRunner
 from .script_edit_dialog import ScriptEditDialog
 from .script_run_dialog import ScriptRunDialog
@@ -33,8 +33,8 @@ class ScriptControl(QGroupBox):
         edit_btn = QPushButton("Edit script")
         edit_btn.clicked.connect(self._edit_btn_clicked)
 
-        self.script_path = OpenDirectoryWidget(
-            initial_dir_path=_get_previous_script_path(),
+        self.script_path = OpenPathWidget(
+            initial_file_path=_get_previous_script_path(),
             extension="yaml",
             parent=self,
             caption="Choose measure script to load",