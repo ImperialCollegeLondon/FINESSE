6021cda6f428531a941c506e9adb3eae2fabdd43
Alex Dewar <a.dewar@imperial.ac.uk>
2023-10-03 14:37:04 +0100

    Allow for devices to have names

--- finesse/hardware/device_base.py
+++ finesse/hardware/device_base.py
@@ -9,6 +9,32 @@ class DeviceBase(ABC):
     """A string to be used in pubsub topics etc. which describes the base type."""
     _device_description: str
     """A human-readable name."""
+    _device_names: set[str] | None
+    """Possible names for this device (None == any name)."""
+
+    def __init__(self, name: str | None = None) -> None:
+        """Create a new DeviceBase.
+
+        Args:
+            name: A name to distinguish devices of the same type.
+        """
+        self.topic = f"serial.{self._device_base_type}"
+        """The name of the root pubsub topic on which this device will broadcast."""
+
+        self.name = name
+        """The (optional) name of this device to use in pubsub messages."""
+
+        if not self._device_names:
+            if name:
+                raise RuntimeError(
+                    "Name provided for device which cannot accept names."
+                )
+            return
+
+        if name not in self._device_names:
+            raise RuntimeError("Invalid name given for device")
+
+        self.topic += f".{name}"
 
     def __del__(self) -> None:
         """Close the device on deletion."""
