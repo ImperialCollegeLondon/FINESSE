807ac1d891c44f3b600b1b1b49a74021645dcddf
Alex Dewar <a.dewar@imperial.ac.uk>
2023-10-06 12:24:11 +0100

    Fix communication with temperature monitor backend
    Some of this code was lost during the refactor.

--- finesse/hardware/__init__.py
+++ finesse/hardware/__init__.py
@@ -1,5 +1,6 @@
 """This module contains code for interfacing with different hardware devices."""
 import sys
+from decimal import Decimal
 
 from pubsub import pub
 
@@ -10,10 +11,14 @@ else:
     from .em27_scraper import EM27Scraper  # type: ignore
     from .opus.em27 import OPUSInterface  # type: ignore
 
+from datetime import datetime
+
+from finesse.config import NUM_TEMPERATURE_MONITOR_CHANNELS, TEMPERATURE_MONITOR_TOPIC
 from finesse.device_info import DeviceBaseTypeInfo, DeviceTypeInfo
 
 from . import data_file_writer  # noqa: F401
 from .plugins import load_device_types
+from .plugins.temperature import get_temperature_monitor_instance
 
 _opus: OPUSInterface
 
@@ -49,6 +54,39 @@ def _get_device_type_info() -> dict[DeviceBaseTypeInfo, list[DeviceTypeInfo]]:
 def _broadcast_device_types() -> None:
     """Broadcast the available device types via pubsub."""
     pub.sendMessage("device.list", device_types=_get_device_type_info())
+
+
+def _try_get_temperatures() -> list[Decimal] | None:
+    """Try to read the current temperatures from the temperature monitor.
+
+    If the device is not connected or the operation fails, None is returned.
+    """
+    dev = get_temperature_monitor_instance()
+    if not dev:
+        return None
+
+    try:
+        return dev.get_temperatures()
+    except Exception as error:
+        pub.sendMessage(f"device.error.{TEMPERATURE_MONITOR_TOPIC}", error=error)
+        return None
+
+
+_DEFAULT_TEMPS = [Decimal("nan")] * NUM_TEMPERATURE_MONITOR_CHANNELS
+
+
+def _send_temperatures() -> None:
+    """Send the current temperatures (or NaNs) via pubsub."""
+    temperatures = _try_get_temperatures() or _DEFAULT_TEMPS
+    time = datetime.utcnow()
+    pub.sendMessage(
+        f"device.{TEMPERATURE_MONITOR_TOPIC}.data.response",
+        temperatures=temperatures,
+        time=time,
+    )
+
+
+pub.subscribe(_send_temperatures, f"device.{TEMPERATURE_MONITOR_TOPIC}.data.request")
 
 
 def _init_hardware():
