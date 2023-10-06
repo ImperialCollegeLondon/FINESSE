0bd0481a856904409979b4d6d82196bb44594121
Alex Dewar <a.dewar@imperial.ac.uk>
2023-10-06 12:24:11 +0100

    Fix communication with temperature monitor backend
    Some of this code was lost during the refactor.

--- finesse/hardware/plugins/temperature/__init__.py
+++ finesse/hardware/plugins/temperature/__init__.py
@@ -1,11 +1,12 @@
 """This module contains interfaces for temperature-related hardware."""
 from typing import cast
 
-from finesse.config import TEMPERATURE_CONTROLLER_TOPIC
+from finesse.config import TEMPERATURE_CONTROLLER_TOPIC, TEMPERATURE_MONITOR_TOPIC
 from finesse.device_info import DeviceInstanceRef
 from finesse.hardware.devices import devices
 
 from .temperature_controller_base import TemperatureControllerBase
+from .temperature_monitor_base import TemperatureMonitorBase
 
 
 def get_temperature_controller_instance(name: str) -> TemperatureControllerBase | None:
@@ -18,6 +19,17 @@ def get_temperature_controller_instance(name: str) -> TemperatureControllerBase 
         return cast(
             TemperatureControllerBase,
             devices[DeviceInstanceRef(TEMPERATURE_CONTROLLER_TOPIC, name)],
+        )
+    except KeyError:
+        return None
+
+
+def get_temperature_monitor_instance() -> TemperatureMonitorBase | None:
+    """Get the current temperature monitor device or None."""
+    try:
+        return cast(
+            TemperatureMonitorBase,
+            devices[DeviceInstanceRef(TEMPERATURE_MONITOR_TOPIC)],
         )
     except KeyError:
         return None
