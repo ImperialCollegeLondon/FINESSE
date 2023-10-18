ea9a23647947b0eab5a38ba60043d853de1e01b9
CWestICL <cwest@ic.ac.uk>
2023-10-18 14:25:31 +0100

    Changed to subprocess.run()

--- docs/gen_user_guide.py
+++ docs/gen_user_guide.py
@@ -1,7 +1,6 @@
 """Generate the User Guide."""
-import os
+import subprocess
 
-os.system(
-    r"""pandoc -f markdown -t html5 -o user_guide.html docs\user_guide.md -c style
-    .css"""
+subprocess.run(
+    "pandoc -f markdown -t html5 -o user_guide.html docs/user_guide.md", check=True
 )
