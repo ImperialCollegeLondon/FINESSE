5dfec1cf1b04c89cfe681fb645a02864c961dc93
CWestICL <cwest@ic.ac.uk>
2023-10-19 10:05:55 +0100

    pathlib file path

--- docs/gen_user_guide.py
+++ docs/gen_user_guide.py
@@ -1,6 +1,17 @@
 """Generate the User Guide."""
 import subprocess
+from pathlib import Path
 
 subprocess.run(
-    "pandoc -f markdown -t html5 -o user_guide.html docs/user_guide.md", check=True
+    [
+        "pandoc",
+        "-f",
+        "markdown",
+        "-t",
+        "html5",
+        "-o",
+        "user_guide.html",
+        f"{Path(__file__).parent}/user_guide.md",
+    ],
+    check=True,
 )
