ddfb463c88694777717ac978cd33c5f5e80f22b0
Alex Dewar <a.dewar@imperial.ac.uk>
2023-11-23 15:39:58 +0000

    Fix: Settings file not created correctly

--- finesse/settings.py
+++ finesse/settings.py
@@ -5,5 +5,5 @@ from finesse.config import APP_CONFIG_PATH
 
 # We use platformdirs to choose the path for config file because Qt seems to use a
 # slightly different scheme (at least on Linux)
-settings = QSettings(str(APP_CONFIG_PATH / "settings.conf"))
+settings = QSettings(str(APP_CONFIG_PATH / "settings.ini"), QSettings.Format.IniFormat)
 """Contains the program settings for FINESSE."""
