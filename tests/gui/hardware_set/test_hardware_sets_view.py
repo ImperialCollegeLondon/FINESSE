5cdb5084fa00a16e67131882853f6a64d71b165e
Alex Dewar <a.dewar@imperial.ac.uk>
2023-11-24 10:25:27 +0000

    Put example HardwareSets for tests into fixture so it can be shared

--- tests/gui/hardware_set/test_hardware_sets_view.py
+++ tests/gui/hardware_set/test_hardware_sets_view.py
@@ -9,50 +9,22 @@ import pytest
 from finesse.gui.hardware_set.hardware_set import HardwareSet, OpenDeviceArgs
 from finesse.gui.hardware_set.hardware_sets_view import HardwareSetsControl
 
-HW_SETS = [
-    HardwareSet(
-        "Test 1",
-        frozenset(
-            (
-                OpenDeviceArgs.create("stepper_motor", "MyStepperMotor"),
-                OpenDeviceArgs.create(
-                    "temperature_monitor",
-                    "MyTemperatureMonitor",
-                    {"param1": "value1"},
-                ),
-            )
-        ),
-        Path("path/test.yaml"),
-        False,
-    ),
-    HardwareSet(
-        "Test 2",
-        frozenset(
-            (
-                OpenDeviceArgs.create("stepper_motor", "OtherStepperMotor"),
-                OpenDeviceArgs.create("temperature_monitor", "OtherTemperatureMonitor"),
-            )
-        ),
-        Path("path/test2.yaml"),
-        False,
-    ),
-]
-
 
 @pytest.fixture
 @patch.object(HardwareSetsControl, "_update_control_state")
 @patch.object(HardwareSetsControl, "_load_last_selected_hardware_set")
 @patch("finesse.gui.hardware_set.hardware_sets_view.get_hardware_sets")
-def hw_sets(
+def hw_control(
     get_hw_sets_mock: Mock,
     update_mock: Mock,
     load_mock: Mock,
+    hw_sets: Sequence[HardwareSet],
     sendmsg_mock: MagicMock,
     subscribe_mock: MagicMock,
     qtbot,
 ) -> HardwareSetsControl:
     """A fixture for the control."""
-    get_hw_sets_mock.return_value = iter(HW_SETS)
+    get_hw_sets_mock.return_value = iter(hw_sets)
     return HardwareSetsControl()
 
 
@@ -92,24 +64,24 @@ def test_load_last_selected_hardware_set(
     settings_mock: Mock,
     selected_hw_set: str,
     expected_selection: str | None,
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     qtbot,
 ) -> None:
     """Test the _load_last_selected_hardware_set() method."""
     settings_mock.value.return_value = selected_hw_set
-    hw_sets._load_last_selected_hardware_set()
+    hw_control._load_last_selected_hardware_set()
     settings_mock.value.assert_called_once_with("hardware_set/selected")
-    assert hw_sets._hardware_sets_combo.currentText() == expected_selection
+    assert hw_control._hardware_sets_combo.currentText() == expected_selection
 
 
 @patch("finesse.gui.hardware_set.hardware_sets_view.get_hardware_sets")
 def test_load_hardware_set_list(
-    get_hw_sets_mock: Mock, hw_sets: HardwareSetsControl, qtbot
+    get_hw_sets_mock: Mock, hw_control: HardwareSetsControl, qtbot
 ) -> None:
     """Test the _load_hardware_set_list() method."""
     get_hw_sets_mock.return_value = range(2)
-    with patch.object(hw_sets, "_add_hardware_set") as add_mock:
-        hw_sets._load_hardware_set_list()
+    with patch.object(hw_control, "_add_hardware_set") as add_mock:
+        hw_control._load_hardware_set_list()
         add_mock.assert_has_calls((call(0), call(1)))
 
 
@@ -120,7 +92,7 @@ def test_import_hardware_set_success(
     open_file_mock: Mock,
     error_message_mock: Mock,
     load_mock: Mock,
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     sendmsg_mock: MagicMock,
     qtbot,
 ) -> None:
@@ -129,7 +101,7 @@ def test_import_hardware_set_success(
     hw_set = MagicMock()
     load_mock.return_value = hw_set
     open_file_mock.return_value = (str(path), None)
-    hw_sets._import_hardware_set()
+    hw_control._import_hardware_set()
     load_mock.assert_called_once_with(path)
     sendmsg_mock.assert_called_once_with("hardware_set.add", hw_set=hw_set)
     error_message_mock.assert_not_called()
@@ -142,13 +114,13 @@ def test_import_hardware_set_cancelled(
     open_file_mock: Mock,
     error_message_mock: Mock,
     load_mock: Mock,
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     sendmsg_mock: MagicMock,
     qtbot,
 ) -> None:
     """Test the _import_hardware_set() method when the dialog is closed."""
     open_file_mock.return_value = (None, None)
-    hw_sets._import_hardware_set()
+    hw_control._import_hardware_set()
     sendmsg_mock.assert_not_called()
     error_message_mock.assert_not_called()
     load_mock.assert_not_called()
@@ -161,7 +133,7 @@ def test_import_hardware_set_error(
     open_file_mock: Mock,
     error_message_mock: Mock,
     load_mock: Mock,
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     sendmsg_mock: MagicMock,
     qtbot,
 ) -> None:
@@ -169,24 +141,41 @@ def test_import_hardware_set_error(
     path = Path("dir/file.txt")
     load_mock.side_effect = RuntimeError
     open_file_mock.return_value = (str(path), None)
-    hw_sets._import_hardware_set()
+    hw_control._import_hardware_set()
     load_mock.assert_called_once_with(path)
     sendmsg_mock.assert_not_called()
     error_message_mock.assert_called_once()
 
 
+_HW_SET = HardwareSet(
+    "Test 1",
+    frozenset(
+        (
+            OpenDeviceArgs.create("stepper_motor", "MyStepperMotor"),
+            OpenDeviceArgs.create(
+                "temperature_monitor",
+                "MyTemperatureMonitor",
+                {"param1": "value1"},
+            ),
+        )
+    ),
+    Path("path/test.yaml"),
+    False,
+)
+
+
 @pytest.mark.parametrize(
     "existing_hw_sets,hw_set_name,expected_name,built_in",
     (
-        ((), HW_SETS[0].name, HW_SETS[0].name, False),
-        ((HW_SETS[0],), HW_SETS[0].name, f"{HW_SETS[0].name} (2)", False),
-        ((HW_SETS[0], HW_SETS[0]), HW_SETS[0].name, f"{HW_SETS[0].name} (3)", False),
-        ((), HW_SETS[0].name, f"{HW_SETS[0].name} (built in)", True),
-        ((HW_SETS[0],), HW_SETS[0].name, f"{HW_SETS[0].name} (built in) (2)", True),
+        ((), _HW_SET.name, _HW_SET.name, False),
+        ((_HW_SET,), _HW_SET.name, f"{_HW_SET.name} (2)", False),
+        ((_HW_SET, _HW_SET), _HW_SET.name, f"{_HW_SET.name} (3)", False),
+        ((), _HW_SET.name, f"{_HW_SET.name} (built in)", True),
+        ((_HW_SET,), _HW_SET.name, f"{_HW_SET.name} (built in) (2)", True),
         (
-            (HW_SETS[0], HW_SETS[0]),
-            HW_SETS[0].name,
-            f"{HW_SETS[0].name} (built in) (3)",
+            (_HW_SET, _HW_SET),
+            _HW_SET.name,
+            f"{_HW_SET.name} (built in) (3)",
             True,
         ),
     ),
@@ -196,48 +185,51 @@ def test_add_hardware_set(
     hw_set_name: str,
     expected_name: str,
     built_in: bool,
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     qtbot,
 ) -> None:
     """Test the _add_hardware_set() method."""
     # Patch this function, because it'll break if the combo box is empty
-    with patch.object(hw_sets, "_update_control_state"):
-        hw_sets._hardware_sets_combo.clear()
+    with patch.object(hw_control, "_update_control_state"):
+        hw_control._hardware_sets_combo.clear()
         for hw_set in existing_hw_sets:
             hw_set = HardwareSet(
                 hw_set.name, hw_set.devices, hw_set.file_path, built_in
             )
-            hw_sets._add_hardware_set(hw_set)
+            hw_control._add_hardware_set(hw_set)
 
-        with patch.object(hw_sets._hardware_sets_combo, "addItem") as add_mock:
+        with patch.object(hw_control._hardware_sets_combo, "addItem") as add_mock:
             hw_set = HardwareSet(hw_set_name, frozenset(), Path(), built_in)
-            hw_sets._add_hardware_set(hw_set)
+            hw_control._add_hardware_set(hw_set)
             add_mock.assert_called_once_with(expected_name, hw_set)
 
 
 @patch.object(HardwareSetsControl, "_load_hardware_set_list")
 def test_on_hardware_set_added(
-    load_mock: Mock, hw_sets: HardwareSetsControl, qtbot
+    load_mock: Mock,
+    hw_control: HardwareSetsControl,
+    hw_sets: Sequence[HardwareSet],
+    qtbot,
 ) -> None:
     """Test the _on_hardware_set_added() method."""
-    with patch.object(hw_sets, "_hardware_sets_combo") as combo_mock:
-        combo_mock.itemData = lambda idx: HW_SETS[idx]
-        combo_mock.count.return_value = len(HW_SETS)
-        hw_sets._on_hardware_set_added(HW_SETS[1])
+    with patch.object(hw_control, "_hardware_sets_combo") as combo_mock:
+        combo_mock.itemData = lambda idx: hw_sets[idx]
+        combo_mock.count.return_value = len(hw_sets)
+        hw_control._on_hardware_set_added(hw_sets[1])
         load_mock.assert_called_once_with()
         combo_mock.setCurrentIndex.assert_called_once_with(1)
 
 
-def test_current_hardware_set(hw_sets: HardwareSetsControl, qtbot) -> None:
+def test_current_hardware_set(hw_control: HardwareSetsControl, qtbot) -> None:
     """Test the current_hardware_set property."""
-    with patch.object(hw_sets._hardware_sets_combo, "currentData") as data_mock:
+    with patch.object(hw_control._hardware_sets_combo, "currentData") as data_mock:
         hw_set = MagicMock()
         data_mock.return_value = hw_set
-        assert hw_sets.current_hardware_set is hw_set.devices
+        assert hw_control.current_hardware_set is hw_set.devices
 
         # Should also work if no hardware set is selected
         data_mock.return_value = None
-        assert hw_sets.current_hardware_set == frozenset()
+        assert hw_control.current_hardware_set == frozenset()
 
 
 DEVICES = [
@@ -264,7 +256,7 @@ def test_update_control_state(
     disconnect_enabled: bool,
     connected_devices: Sequence[int],
     hardware_set: Sequence[int],
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     sendmsg_mock: MagicMock,
     qtbot,
 ) -> None:
@@ -276,15 +268,15 @@ def test_update_control_state(
     ) as hw_set_mock:
         hw_set_mock.return_value = _get_devices(hardware_set)
         with patch.object(
-            hw_sets, "_connected_devices", _get_devices(connected_devices)
+            hw_control, "_connected_devices", _get_devices(connected_devices)
         ):
             with patch.object(
-                hw_sets._connect_btn, "setEnabled"
+                hw_control._connect_btn, "setEnabled"
             ) as connect_enable_mock:
                 with patch.object(
-                    hw_sets._disconnect_btn, "setEnabled"
+                    hw_control._disconnect_btn, "setEnabled"
                 ) as disconnect_enable_mock:
-                    hw_sets._update_control_state()
+                    hw_control._update_control_state()
                     connect_enable_mock.assert_called_once_with(connect_enabled)
                     disconnect_enable_mock.assert_called_once_with(disconnect_enabled)
 
@@ -301,7 +293,7 @@ def test_connect_btn(
     connected_devices: Sequence[int],
     hardware_set: Sequence[int],
     open_called: Sequence[int],
-    hw_sets: HardwareSetsControl,
+    hw_control: HardwareSetsControl,
     qtbot,
 ) -> None:
     """Test the connect button."""
@@ -312,9 +304,9 @@ def test_connect_btn(
     ) as hw_set_mock:
         hw_set_mock.return_value = _get_devices(hardware_set)
         with patch.object(
-            hw_sets, "_connected_devices", _get_devices(connected_devices)
+            hw_control, "_connected_devices", _get_devices(connected_devices)
         ):
-            hw_sets._connect_btn.click()
+            hw_control._connect_btn.click()
 
             open_mock.assert_has_calls(
                 list(
@@ -331,29 +323,29 @@ def test_connect_btn(
 
 @patch("finesse.gui.hardware_set.hardware_set.close_device")
 def test_disconnect_button(
-    close_mock: Mock, hw_sets: HardwareSetsControl, qtbot
+    close_mock: Mock, hw_control: HardwareSetsControl, qtbot
 ) -> None:
     """Test the disconnect button."""
-    with patch.object(hw_sets, "_update_control_state") as update_mock:
-        with patch.object(hw_sets, "_connected_devices", DEVICES):
-            hw_sets._disconnect_btn.setEnabled(True)
-            hw_sets._disconnect_btn.click()
+    with patch.object(hw_control, "_update_control_state") as update_mock:
+        with patch.object(hw_control, "_connected_devices", DEVICES):
+            hw_control._disconnect_btn.setEnabled(True)
+            hw_control._disconnect_btn.click()
             close_mock.assert_has_calls([call(device.instance) for device in DEVICES])
             update_mock.assert_called_once_with()
 
 
 @patch("finesse.gui.hardware_set.hardware_sets_view.settings")
 def test_on_device_opened(
-    settings_mock: Mock, hw_sets: HardwareSetsControl, qtbot
+    settings_mock: Mock, hw_control: HardwareSetsControl, qtbot
 ) -> None:
     """Test the _on_device_opened() method."""
     device = DEVICES[0]
-    assert not hw_sets._connected_devices
-    with patch.object(hw_sets, "_update_control_state") as update_mock:
-        hw_sets._on_device_opened(
+    assert not hw_control._connected_devices
+    with patch.object(hw_control, "_update_control_state") as update_mock:
+        hw_control._on_device_opened(
             instance=device.instance, class_name=device.class_name, params=device.params
         )
-        assert hw_sets._connected_devices == {device}
+        assert hw_control._connected_devices == {device}
         update_mock.assert_called_once_with()
         settings_mock.setValue.assert_has_calls(
             [
@@ -363,46 +355,46 @@ def test_on_device_opened(
         )
 
 
-def test_on_device_closed(hw_sets: HardwareSetsControl, qtbot) -> None:
+def test_on_device_closed(hw_control: HardwareSetsControl, qtbot) -> None:
     """Test the _on_device_closed() method."""
     device = DEVICES[0]
-    assert not hw_sets._connected_devices
-    with patch.object(hw_sets, "_update_control_state") as update_mock:
-        hw_sets._connected_devices.add(device)
-        hw_sets._on_device_closed(device.instance)
-        assert not hw_sets._connected_devices
+    assert not hw_control._connected_devices
+    with patch.object(hw_control, "_update_control_state") as update_mock:
+        hw_control._connected_devices.add(device)
+        hw_control._on_device_closed(device.instance)
+        assert not hw_control._connected_devices
         update_mock.assert_called_once_with()
 
 
-def test_on_device_closed_not_found(hw_sets: HardwareSetsControl, qtbot) -> None:
+def test_on_device_closed_not_found(hw_control: HardwareSetsControl, qtbot) -> None:
     """Test that _on_device_closed() does not raise an error if device is not found."""
     device = DEVICES[0]
-    assert not hw_sets._connected_devices
+    assert not hw_control._connected_devices
     with does_not_raise():
-        hw_sets._on_device_closed(device.instance)
+        hw_control._on_device_closed(device.instance)
 
 
-def test_show_manage_devices_dialog(hw_sets: HardwareSetsControl, qtbot) -> None:
+def test_show_manage_devices_dialog(hw_control: HardwareSetsControl, qtbot) -> None:
     """Test the _show_manage_devices_dialog() method."""
     # Check that the dialog is created if it doesn't exist
-    assert not hasattr(hw_sets, "_manage_devices_dialog")
-    hw_sets._show_manage_devices_dialog()
-    dialog = hw_sets._manage_devices_dialog
+    assert not hasattr(hw_control, "_manage_devices_dialog")
+    hw_control._show_manage_devices_dialog()
+    dialog = hw_control._manage_devices_dialog
     assert not dialog.isHidden()
 
     # If it already exists, check it is shown
     dialog.hide()
-    hw_sets._show_manage_devices_dialog()
+    hw_control._show_manage_devices_dialog()
     assert not dialog.isHidden()
-    assert hw_sets._manage_devices_dialog is dialog
+    assert hw_control._manage_devices_dialog is dialog
 
 
 def test_remove_hw_set_btn(
-    hw_sets: HardwareSetsControl, sendmsg_mock: Mock, qtbot
+    hw_control: HardwareSetsControl, sendmsg_mock: Mock, qtbot
 ) -> None:
     """Test that _remove_hw_set_btn works."""
-    with patch.object(hw_sets._hardware_sets_combo, "currentData") as data_mock:
+    with patch.object(hw_control._hardware_sets_combo, "currentData") as data_mock:
         hw_set = MagicMock()
         data_mock.return_value = hw_set
-        hw_sets._remove_hw_set_btn.click()
+        hw_control._remove_hw_set_btn.click()
         sendmsg_mock.assert_called_once_with("hardware_set.remove", hw_set=hw_set)
