05915763991a0b52343095c53575b02bab77cdf2
CWestICL <cwest@ic.ac.uk>
2023-11-29 12:03:42 +0000

    Added .read() tests

--- tests/hardware/plugins/temperature/test_senecaK107.py
+++ tests/hardware/plugins/temperature/test_senecaK107.py
@@ -1,10 +1,9 @@
 """Tests for the Seneca K107 device."""
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
-import numpy
 import pytest
 
-from finesse.hardware.plugins.temperature.senecak107 import SenecaK107
+from finesse.hardware.plugins.temperature.senecak107 import SenecaK107, SenecaK107Error
 
 _SERIAL_ARGS = ("0403:6001 AB0LMVI5A", 57600)
 
@@ -44,11 +43,40 @@ def test_write(dev: SenecaK107) -> None:
     dev.serial.write.assert_called_once_with(bytearray([1, 3, 0, 2, 0, 8, 229, 204]))
 
 
+def test_write_error(dev: SenecaK107) -> None:
+    """Test SenecaK107.write() error handling."""
+    dev.serial.write.side_effect = RuntimeError
+    with pytest.raises(SenecaK107Error):
+        dev.request_read()
+
+
+def test_read(dev: SenecaK107, data: bytes) -> None:
+    """Test SenecaK107.read()."""
+    with patch.object(dev.serial, "read") as mock:
+        mock.return_value = data
+        assert data == dev.read()
+        mock.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "message",
+    (
+        b"\x01\x03\x101d1p\xff\xfa\xff\xf81u\xff\xfa1d\xff",
+        b"\x01\x03\x101d1p\xff\xfa\xff\xf81u\xff\xfa1d\xff\xfa]Z\x01\x03",
+    ),
+)
+def test_read_error(dev: SenecaK107, message: bytes) -> None:
+    """Test SenecaK107.read() error handling."""
+    with pytest.raises(SenecaK107Error):
+        with patch.object(dev.serial, "read") as mock:
+            mock.return_value = message
+            dev.read()
+
+
 def test_parse_data(dev: SenecaK107, data: bytes) -> None:
     """Test SenecaK107.parse_data()."""
     parsed = dev.parse_data(data)
 
-    assert parsed[0] is numpy.float64
     assert parsed == [
         19.946250000000006,
         20.085000000000008,
