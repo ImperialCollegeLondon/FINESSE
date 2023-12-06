1c279ba5225e96c6679a8c26b4f51984f0506a52
Alex Dewar <a.dewar@imperial.ac.uk>
2023-12-06 17:47:49 +0000

    SpectrometerControl: Replace log view with status widget
    Closes #396.

--- finesse/gui/spectrometer_view.py
+++ finesse/gui/spectrometer_view.py
@@ -1,23 +1,12 @@
 """Panel and widgets related to the control of spectrometers."""
-import logging
-import weakref
 from collections.abc import Sequence
 from functools import partial
 
 from frozendict import frozendict
 from pubsub import pub
-from PySide6.QtGui import QTextCursor
-from PySide6.QtWidgets import (
-    QGroupBox,
-    QHBoxLayout,
-    QPushButton,
-    QSizePolicy,
-    QTextBrowser,
-    QVBoxLayout,
-)
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QSizePolicy, QVBoxLayout
 
 from finesse.config import SPECTROMETER_TOPIC
-from finesse.device_info import DeviceInstanceRef
 from finesse.gui.device_panel import DevicePanel
 from finesse.spectrometer_status import SpectrometerStatus
 
@@ -25,7 +14,7 @@ from finesse.spectrometer_status import SpectrometerStatus
 class SpectrometerControl(DevicePanel):
     """Class to monitor and control spectrometers."""
 
-    COMMANDS = ("connect", "start", "stop", "cancel")
+    _COMMANDS = ("connect", "start", "stop", "cancel")
     """The default commands shown."""
 
     _ENABLED_BUTTONS = frozendict(
@@ -37,49 +26,36 @@ class SpectrometerControl(DevicePanel):
     )
     """Which buttons to enable for different states."""
 
-    def __init__(self, commands: Sequence[str] = COMMANDS) -> None:
+    def __init__(self, commands: Sequence[str] = _COMMANDS) -> None:
         """Create the widgets to monitor and control the spectrometer."""
         super().__init__(SPECTROMETER_TOPIC, "Spectrometer")
 
-        self.commands = commands
-        self.logger = logging.getLogger("spectrometer")
-        self.buttons: dict[str, QPushButton] = {}
+        self._buttons: dict[str, QPushButton] = {}
+        """Buttons for the different spectrometer commands."""
+        self._status_label = QLabel("Status:")
+        """A label showing the current status of the device."""
 
-        layout = self._create_controls()
+        layout = QVBoxLayout()
+        layout.addLayout(self._create_buttons(commands))
+        layout.addWidget(self._status_label)
         self.setLayout(layout)
 
         self.setSizePolicy(
-            QSizePolicy.Policy.Preferred,
+            QSizePolicy.Policy.Expanding,
             QSizePolicy.Policy.MinimumExpanding,
         )
 
-        pub.subscribe(self._log_request, f"device.{SPECTROMETER_TOPIC}.request")
-        pub.subscribe(self._log_response, f"device.{SPECTROMETER_TOPIC}.status")
-        pub.subscribe(
-            self._update_buttons_enabled, f"device.{SPECTROMETER_TOPIC}.status"
-        )
-        pub.subscribe(self._log_error, f"device.error.{SPECTROMETER_TOPIC}")
+        pub.subscribe(self._on_status_changed, f"device.{SPECTROMETER_TOPIC}.status")
 
-    def _create_controls(self) -> QHBoxLayout:
-        """Creates the controls for communicating with the interferometer.
-
-        Returns:
-            The layout with the buttons
-        """
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(self._create_log_area())
-        main_layout.addLayout(self._create_buttons())
-        return main_layout
-
-    def _create_buttons(self) -> QVBoxLayout:
+    def _create_buttons(self, commands: Sequence[str]) -> QHBoxLayout:
         """Creates the buttons.
 
         Returns:
             The layout with the buttons
         """
-        btn_layout = QVBoxLayout()
+        btn_layout = QHBoxLayout()
 
-        for name in self.commands:
+        for name in commands:
             button = QPushButton(name.capitalize())
             button.setEnabled(False)
             button.clicked.connect(
@@ -87,42 +63,17 @@ class SpectrometerControl(DevicePanel):
             )
             btn_layout.addWidget(button)
 
-            self.buttons[name] = button
-
-        btn_layout.addStretch()
+            self._buttons[name] = button
 
         return btn_layout
 
-    def _create_log_area(self) -> QGroupBox:
-        """Creates the log area for spectrometer-related communication.
+    def _on_status_changed(self, status: SpectrometerStatus) -> None:
+        """Update the GUI based on the status."""
+        self._status_label.setText(f"Status: <b>{status.name}</b>")
 
-        Returns:
-            Widget containing the log area
-        """
-        log_box = QGroupBox("Error log")
-        log_area = QTextBrowser()
-        LogHandler.set_handler(self.logger, log_area)
-
-        log_layout = QVBoxLayout()
-        log_layout.addWidget(log_area)
-        log_box.setLayout(log_layout)
-
-        return log_box
-
-    def _log_request(self, command: str) -> None:
-        """Log when a command request is sent."""
-        self.logger.info(f'Executing command "{command}"')
-
-    def _log_response(self, status: SpectrometerStatus) -> None:
-        self.logger.info(f"Status: {status.name}")
-
-    def _log_error(self, instance: DeviceInstanceRef, error: BaseException) -> None:
-        self.logger.error(f"Error during request: {error!s}")
-
-    def _update_buttons_enabled(self, status: SpectrometerStatus) -> None:
-        """Enable/disable buttons depending on the spectrometer's status."""
+        # Enable/disable buttons depending on the spectrometer's status
         to_enable = self._ENABLED_BUTTONS.get(status, set())
-        for name, btn in self.buttons.items():
+        for name, btn in self._buttons.items():
             btn.setEnabled(name in to_enable)
 
     def on_command_button_clicked(self, command: str) -> None:
@@ -132,49 +83,3 @@ class SpectrometerControl(DevicePanel):
             command: Command to be executed
         """
         pub.sendMessage(f"device.{SPECTROMETER_TOPIC}.request", command=command)
-
-
-class LogHandler(logging.Handler):
-    """Specific logger for spectrometers."""
-
-    @classmethod
-    def set_handler(cls, logger: logging.Logger, log_area: QTextBrowser):
-        """Creates the handler and adds it to the logger.
-
-        Args:
-            logger: The logger to set the formatter for
-            log_area: The area where the log will be printed.
-        """
-        ch = cls(weakref.ref(log_area))
-
-        formatter = logging.Formatter(
-            fmt="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y/%m/%d %H:%M:%S"
-        )
-        ch.setFormatter(formatter)
-
-        logger.addHandler(ch)
-
-    def __init__(self, log_area: weakref.ref):
-        """Constructor of the Handler.
-
-        Args:
-            log_area: A weak reference to the log area.
-        """
-        super().__init__()
-        self.log_area = log_area
-
-    def emit(self, record):
-        """Add the record to the text area.
-
-        If the log area has been destroyed before the logger, it will raise an
-        AttributeError. This can only happen during tests and can be safely ignored.
-        """
-        log_area = self.log_area()
-        if not log_area:
-            # log_area no longer exists
-            return
-
-        log_area.append(self.format(record))
-        cursor = log_area.textCursor()
-        cursor.movePosition(QTextCursor.End)
-        log_area.setTextCursor(cursor)
