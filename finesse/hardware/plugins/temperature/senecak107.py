5b5db2308df383f2719ff299ef7ca9b8b2eed89e
CWestICL <cwest@ic.ac.uk>
2024-01-22 15:19:38 +0000

    Ignore Crc mypy

--- finesse/hardware/plugins/temperature/senecak107.py
+++ finesse/hardware/plugins/temperature/senecak107.py
@@ -27,7 +27,7 @@ def calculate_crc(data: bytes) -> int:
     Returns:
         bcc: The calculated checksum
     """
-    calculator = Calculator(Crc16.MODBUS)
+    calculator = Calculator(Crc16.MODBUS)  # type: ignore
     checksum = calculator.checksum(data[:19])
     return checksum
 
