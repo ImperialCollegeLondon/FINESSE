8a352f05c0779dc927e2853398ef06a9a035220a
James Paul Turner <james.turner@imperial.ac.uk>
2024-02-15 15:30:33 +0000

    Use correct endpoint.

--- finesse/config.py
+++ finesse/config.py
@@ -56,14 +56,21 @@ EM27_SENSORS_TOPIC = "em27_sensors"
 DECADES_HOST = "localhost"
 """The IP address or hostname of the DECADES server."""
 
-DECADES_SENSORS_URL = "http://{host}/decades/params"
-"""The URL of the DECADES server."""
+DECADES_SENSORS_URL = "http://{host}/decades/livedata"
+"""The URL of the DECADES sensor data endpoint."""
 
 DECADES_SENSORS_POLL_INTERVAL = 60.0
 """Poll rate for DECADES sensors."""
 
 DECADES_SENSORS_TOPIC = "decades_sensors"
 """The topic name to use for DECADES sensor-related messages."""
+
+DECADES_SENSORS_QUERY_LIST = [
+    "static_pressure",
+    "gin_altitude",
+    "deiced_true_air_temp_c",
+]
+"""List of DECADES sensor queries to make."""
 
 DEFAULT_HTTP_TIMEOUT = 2.0
 """How long to wait for a response from a server for."""
