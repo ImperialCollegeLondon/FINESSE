5228c0f24e2d95db18fc18c39338f6b87183701a
James Paul Turner <james.turner@imperial.ac.uk>
2024-02-22 13:18:38 +0000

    Add a plugin NTP server time device.

---
+++ finesse/hardware/plugins/time/ntp_time.py
@@ -0,0 +1,45 @@
+"""This module provides an interface for querying time from an NTP server."""
+from ntplib import NTPClient
+
+from finesse.config import TIME_NTP_HOST
+from finesse.hardware.plugins.time.time_base import TimeBase
+
+
+class NTPTimeError(Exception):
+    """Indicates that an error occurred while querying the time."""
+
+
+class NTPTime(
+    TimeBase,
+    description="NTP time",
+    parameters={
+        "ntp_host": "The NTP server to query",
+    },
+):
+    """A time source that queries an NTP server."""
+
+    def __init__(self, ntp_host: str = TIME_NTP_HOST) -> None:
+        """Create a new NTPTime."""
+        super().__init__()
+        self._client = NTPClient()
+        self._ntp_host = ntp_host
+        self._ntp_version = 3
+        self._ntp_port = "ntp"
+        self._ntp_timeout = 5
+
+    def close(self) -> None:
+        """Close the connection to the device."""
+
+    def get_time(self) -> float:
+        """Get the current time."""
+        try:
+            response = self._client.request(
+                self._ntp_host,
+                version=self._ntp_version,
+                port=self._ntp_port,
+                timeout=self._ntp_timeout,
+            )
+        except Exception as e:
+            raise NTPTimeError(f"Error querying NTP server: {e}")
+
+        return response.dest_time + response.offset
