0ebc1ea7d6aa6c5a39089afc3e3f7a3384ccc658
James Paul Turner <james.turner@imperial.ac.uk>
2024-02-26 08:42:04 +0000

    Add NTP version, port and timeout args to NTPTime ctor.

--- finesse/hardware/plugins/time/ntp_time.py
+++ finesse/hardware/plugins/time/ntp_time.py
@@ -1,7 +1,12 @@
 """This module provides an interface for querying time from an NTP server."""
 from ntplib import NTPClient
 
-from finesse.config import TIME_NTP_HOST
+from finesse.config import (
+    TIME_NTP_HOST,
+    TIME_NTP_PORT,
+    TIME_NTP_TIMEOUT,
+    TIME_NTP_VERSION,
+)
 from finesse.hardware.plugins.time.time_base import TimeBase
 
 
@@ -14,22 +19,34 @@ class NTPTime(
     description="NTP time",
     parameters={
         "ntp_host": "The NTP server to query",
+        "ntp_version": "The NTP version to use",
+        "ntp_port": "The port to connect to",
+        "ntp_timeout": "The maximum time to wait for a response",
     },
 ):
     """A time source that queries an NTP server."""
 
-    def __init__(self, ntp_host: str = TIME_NTP_HOST) -> None:
+    def __init__(
+        self,
+        ntp_host: str = TIME_NTP_HOST,
+        ntp_version: int = TIME_NTP_VERSION,
+        ntp_port: str = TIME_NTP_PORT,
+        ntp_timeout: float = TIME_NTP_TIMEOUT,
+    ) -> None:
         """Create a new NTPTime.
 
         Args:
             ntp_host: The IP address or hostname of the NTP server
+            ntp_version: The NTP version to use
+            ntp_port: The port to connect to
+            ntp_timeout: The maximum time to wait for a response
         """
         super().__init__()
         self._client = NTPClient()
         self._ntp_host = ntp_host
-        self._ntp_version = 3
-        self._ntp_port = "ntp"
-        self._ntp_timeout = 5
+        self._ntp_version = ntp_version
+        self._ntp_port = ntp_port
+        self._ntp_timeout = ntp_timeout
 
     def close(self) -> None:
         """Close the connection to the device."""
