14627087f5b7fea7cadefc14c84b06843b44214d
James Paul Turner <james.turner@imperial.ac.uk>
2024-03-15 15:29:51 +0000

    Add get_time_instance for getting time device.

--- finesse/hardware/plugins/time/__init__.py
+++ finesse/hardware/plugins/time/__init__.py
@@ -1,1 +1,9 @@
 """This module contains interfaces for querying time sources."""
+
+from finesse.hardware.manage_devices import get_device_instance
+from finesse.hardware.plugins.time.time_base import TimeBase
+
+
+def get_time_instance() -> TimeBase | None:
+    """Get the instance of the current time source if connected or None."""
+    return get_device_instance(TimeBase)
