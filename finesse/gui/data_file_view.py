ffe2b433bac68146b4473e736c477116944b49d3
Alex Dewar <a.dewar@imperial.ac.uk>
2024-04-10 11:49:39 +0100

    DataFileControl: Add label and LED to make it obvious when recording is happening

--- finesse/gui/data_file_view.py
+++ finesse/gui/data_file_view.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import cast
 
 from pubsub import pub
+from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import (
     QGridLayout,
     QGroupBox,
@@ -18,6 +19,7 @@ from PySide6.QtWidgets import (
 )
 
 from finesse.config import DEFAULT_DATA_FILE_PATH
+from finesse.gui.led_icon import LEDIcon
 from finesse.gui.path_widget import OpenDirectoryWidget
 from finesse.settings import settings
 
@@ -29,6 +31,70 @@ def _get_previous_destination_dir() -> Path:
 
 def _get_previous_filename_prefix() -> str:
     return cast(str, settings.value("data/filename_prefix", "data"))
+
+
+class RecordingWidget(QWidget):
+    """A widget to start/stop recording and display recording status.
+
+    When the user clicks the button to start/stop recording the start_recording or
+    stop_recording signals, respectively, are triggered.
+
+    Whether or not data is currently being recorded is shown by a text label and a
+    flashing LED indicator.
+    """
+
+    start_recording = Signal()
+    stop_recording = Signal()
+
+    def __init__(self) -> None:
+        """Create a new RecordingWidget."""
+        super().__init__()
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        self._led = LEDIcon.create_red_icon()
+        """Icon to indicate whether recording is taking place."""
+        pub.subscribe(self._led.flash, "data_file.writing")
+        layout.addWidget(self._led)
+
+        self._label = QLabel("NOT RECORDING")
+        """Label to show recording status."""
+        # Put a margin on the right so that the button isn't too close
+        self._label.setContentsMargins(0, 0, 50, 0)
+        layout.addWidget(self._label)
+
+        self._btn = QPushButton("Start recording")
+        """Send signal to start/stop recording."""
+        self._btn.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum)
+        self._btn.clicked.connect(self._on_button_clicked)
+        layout.addWidget(self._btn)
+
+        self._is_recording = False
+        """Whether data is being recorded."""
+
+        # Update widget on file open/close
+        pub.subscribe(self._on_file_open, "data_file.open")
+        pub.subscribe(self._on_file_close, "data_file.close")
+
+    def _on_file_open(self, path: Path) -> None:
+        """Update widget when recording starts."""
+        self._is_recording = True
+        self._btn.setText("Stop recording")
+        self._label.setText('<font color="red"><b>RECORDING IN PROGRESS</b></font>')
+
+    def _on_file_close(self) -> None:
+        """Update widget when recording stops."""
+        self._is_recording = False
+        self._btn.setText("Start recording")
+        self._label.setText("NOT RECORDING")
+
+    def _on_button_clicked(self) -> None:
+        if self._is_recording:
+            self.stop_recording.emit()
+        else:
+            self.start_recording.emit()
 
 
 class DataFileControl(QGroupBox):
@@ -52,20 +118,19 @@ class DataFileControl(QGroupBox):
 
         layout.addWidget(QLabel("Filename prefix:"), 1, 0)
 
-        filename = QWidget()
-        filename_layout = QHBoxLayout()
-
         self._filename_prefix_widget = QLineEdit()
         self._filename_prefix_widget.setText(_get_previous_filename_prefix())
-        self._record_btn = QPushButton("Start recording")
-        """Toggles recording state."""
-        self._record_btn.clicked.connect(self._toggle_recording)
-        filename_layout.addWidget(self._filename_prefix_widget)
-        filename_layout.addWidget(self._record_btn)
+        layout.addWidget(self._filename_prefix_widget, 1, 1)
 
-        filename.setLayout(filename_layout)
-
-        layout.addWidget(filename, 1, 1)
+        self._record_widget = RecordingWidget()
+        self._record_widget.start_recording.connect(self._try_start_recording)
+        self._record_widget.stop_recording.connect(
+            lambda: pub.sendMessage("data_file.close")
+        )
+        self._record_widget.setSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
+        )
+        layout.addWidget(self._record_widget, 2, 0, 1, 2, Qt.AlignmentFlag.AlignCenter)
 
         self.setLayout(layout)
 
@@ -84,7 +149,6 @@ class DataFileControl(QGroupBox):
     def _on_file_open(self, path: Path) -> None:
         self._open_dir_widget.setEnabled(False)
         self._filename_prefix_widget.setEnabled(False)
-        self._record_btn.setText("Stop recording")
         self._save_file_path_settings()
 
     def _save_file_path_settings(self) -> None:
@@ -97,7 +161,6 @@ class DataFileControl(QGroupBox):
     def _on_file_close(self) -> None:
         self._open_dir_widget.setEnabled(True)
         self._filename_prefix_widget.setEnabled(True)
-        self._record_btn.setText("Start recording")
 
     def _try_get_data_file_path(self) -> Path | None:
         dest_dir = self._open_dir_widget.try_get_path()
@@ -128,11 +191,9 @@ class DataFileControl(QGroupBox):
 
         return path
 
-    def _toggle_recording(self) -> None:
-        """Starts or stops recording as needed."""
-        if self._record_btn.text() == "Stop recording":
-            pub.sendMessage("data_file.close")
-        elif path := self._try_get_data_file_path():
+    def _try_start_recording(self) -> None:
+        """Starts recording if the user has provided a data path."""
+        if path := self._try_get_data_file_path():
             pub.sendMessage("data_file.open", path=path)
 
     def _show_error_message(self, error: BaseException) -> None:
