f9ec2682b27f602ff1fc0f87a6f564e36e7d21bb
Alex Dewar <a.dewar@imperial.ac.uk>
2024-04-10 11:49:39 +0100

    DataFileControl: Add label and LED to make it obvious when recording is happening

--- tests/gui/test_data_file_view.py
+++ tests/gui/test_data_file_view.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from pathlib import Path
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, Mock, call, patch
 
 import pytest
 from freezegun import freeze_time
@@ -20,11 +20,12 @@ def data_file(subscribe_mock: MagicMock, qtbot) -> DataFileControl:
     return DataFileControl()
 
 
-def test_init(subscribe_mock: MagicMock, qtbot) -> None:
+@patch.object(DataFileControl, "_try_start_recording")
+def test_init(
+    start_mock: MagicMock, subscribe_mock: MagicMock, sendmsg_mock: MagicMock, qtbot
+) -> None:
     """Test DataFileControl's constructor."""
     data_file = DataFileControl()
-    assert data_file._record_btn.text() == "Start recording"
-    assert data_file._record_btn.isEnabled()
     assert data_file._open_dir_widget.isEnabled()
     assert data_file._filename_prefix_widget.isEnabled()
 
@@ -32,59 +33,49 @@ def test_init(subscribe_mock: MagicMock, qtbot) -> None:
     subscribe_mock.assert_any_call(data_file._on_file_close, "data_file.close")
     subscribe_mock.assert_any_call(data_file._show_error_message, "data_file.error")
 
+    start_mock.assert_not_called()
+    data_file._record_widget.start_recording.emit()
+    start_mock.assert_called_once_with()
 
-def test_start_recording(
-    data_file: DataFileControl, sendmsg_mock: MagicMock, qtbot
-) -> None:
-    """Test that recording starts correctly."""
-    with patch.object(data_file, "_try_get_data_file_path") as get_path_mock:
-        get_path_mock.return_value = FILE_PATH
-        assert data_file._record_btn.text() == "Start recording"
-        data_file._record_btn.click()
-        sendmsg_mock.assert_called_once_with("data_file.open", path=FILE_PATH)
-
-
-def test_start_recording_path_dialog_cancelled(
-    data_file: DataFileControl, sendmsg_mock: MagicMock, qtbot
-) -> None:
-    """Check that recording isn't started if the user closes the file dialog."""
-    with patch.object(data_file._open_dir_widget, "try_get_path") as path_mock:
-        path_mock.return_value = None
-        data_file._record_btn.click()
-        path_mock.assert_called_once()
-        assert data_file._record_btn.text() == "Start recording"
-        assert data_file._open_dir_widget.isEnabled()
-        sendmsg_mock.assert_not_called()
-
-
-def test_stop_recording(
-    data_file: DataFileControl, sendmsg_mock: MagicMock, qtbot
-) -> None:
-    """Test that recording stops correctly."""
-    # Simulate file open to start with
-    data_file._on_file_open(FILE_PATH)
-
-    data_file._record_btn.click()
+    sendmsg_mock.assert_not_called()
+    data_file._record_widget.stop_recording.emit()
     sendmsg_mock.assert_called_once_with("data_file.close")
+
+
+@patch("finesse.gui.data_file_view.settings")
+def test_save_file_path_settings(
+    settings_mock: Mock, data_file: DataFileControl, qtbot
+) -> None:
+    """Test the _save_file_path_settings() method."""
+    data_file._open_dir_widget.line_edit.setText("/some/path")
+    data_file._filename_prefix_widget.setText("some_prefix")
+    data_file._save_file_path_settings()
+    settings_mock.setValue.assert_has_calls(
+        (
+            call("data/destination_dir", "/some/path"),
+            call("data/filename_prefix", "some_prefix"),
+        )
+    )
 
 
 def test_on_file_open(data_file: DataFileControl, qtbot) -> None:
     """Test the _on_file_open() method."""
-    data_file._on_file_open(FILE_PATH)
-    assert data_file._record_btn.text() == "Stop recording"
-    assert not data_file._open_dir_widget.isEnabled()
+    with patch.object(data_file, "_save_file_path_settings") as save_mock:
+        data_file._open_dir_widget.setEnabled(True)
+        data_file._filename_prefix_widget.setEnabled(True)
+        data_file._on_file_open(FILE_PATH)
+        assert not data_file._open_dir_widget.isEnabled()
+        assert not data_file._filename_prefix_widget.isEnabled()
+        save_mock.assert_called_once_with()
 
 
 def test_on_file_close(data_file: DataFileControl, qtbot) -> None:
     """Test the _on_file_close() method."""
-    # Simulate file open to start with
-    data_file._on_file_open(FILE_PATH)
-
-    # Simulate closing it again
+    data_file._open_dir_widget.setEnabled(False)
+    data_file._filename_prefix_widget.setEnabled(False)
     data_file._on_file_close()
-
-    assert data_file._record_btn.text() == "Start recording"
     assert data_file._open_dir_widget.isEnabled()
+    assert data_file._filename_prefix_widget.isEnabled()
 
 
 @freeze_time(TEST_DATETIME)
@@ -92,18 +83,20 @@ def test_try_get_data_file_path_success(
     data_file: DataFileControl, tmp_path: Path, qtbot
 ) -> None:
     """Test the _try_get_data_path() method when everything succeeds."""
-    data_file.filename_prefix_widget.setText("some_prefix")
+    data_file._filename_prefix_widget.setText("some_prefix")
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     expected_path = tmp_path / f"some_prefix_{timestamp}.csv"
 
-    with patch.object(data_file.open_dir_widget, "try_get_path", return_value=tmp_path):
+    with patch.object(
+        data_file._open_dir_widget, "try_get_path", return_value=tmp_path
+    ):
         assert data_file._try_get_data_file_path() == expected_path
 
 
 def test_try_get_data_file_path_no_data_dir(data_file: DataFileControl, qtbot) -> None:
     """Test the _try_get_data_path() method when data dir not chosen."""
-    data_file.filename_prefix_widget.setText("some_prefix")
-    with patch.object(data_file.open_dir_widget, "try_get_path", return_value=None):
+    data_file._filename_prefix_widget.setText("some_prefix")
+    with patch.object(data_file._open_dir_widget, "try_get_path", return_value=None):
         assert data_file._try_get_data_file_path() is None
 
 
@@ -112,8 +105,10 @@ def test_try_get_data_file_path_no_filename_prefix(
     msgbox_mock: Mock, data_file: DataFileControl, tmp_path: Path, qtbot
 ) -> None:
     """Test the _try_get_data_path() method when filename prefix not entered."""
-    data_file.filename_prefix_widget.setText("")
-    with patch.object(data_file.open_dir_widget, "try_get_path", return_value=tmp_path):
+    data_file._filename_prefix_widget.setText("")
+    with patch.object(
+        data_file._open_dir_widget, "try_get_path", return_value=tmp_path
+    ):
         assert data_file._try_get_data_file_path() is None
         msgbox_mock.assert_called_once()
 
@@ -124,14 +119,35 @@ def test_try_get_data_file_path_file_exists(
     msgbox_mock: Mock, data_file: DataFileControl, tmp_path: Path, qtbot
 ) -> None:
     """Test the _try_get_data_path() method when the file already exists."""
-    data_file.filename_prefix_widget.setText("some_prefix")
+    data_file._filename_prefix_widget.setText("some_prefix")
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     expected_path = tmp_path / f"some_prefix_{timestamp}.csv"
     expected_path.open("a").close()  # create file
 
-    with patch.object(data_file.open_dir_widget, "try_get_path", return_value=tmp_path):
+    with patch.object(
+        data_file._open_dir_widget, "try_get_path", return_value=tmp_path
+    ):
         assert data_file._try_get_data_file_path() is None
         msgbox_mock.assert_called_once()
+
+
+def test_try_start_recording_success(
+    data_file: DataFileControl, sendmsg_mock: Mock, qtbot
+) -> None:
+    """Test that _try_start_recording() sends a pubsub message on success."""
+    path = MagicMock()  # mock path
+    with patch.object(data_file, "_try_get_data_file_path", return_value=path):
+        data_file._try_start_recording()
+        sendmsg_mock.assert_called_once_with("data_file.open", path=path)
+
+
+def test_try_start_recording_fail(
+    data_file: DataFileControl, sendmsg_mock: Mock, qtbot
+) -> None:
+    """Test that _try_start_recording() doesn't send pubsub message on failure."""
+    with patch.object(data_file, "_try_get_data_file_path", return_value=None):
+        data_file._try_start_recording()
+        sendmsg_mock.assert_not_called()
 
 
 @patch("finesse.gui.data_file_view.QMessageBox")
