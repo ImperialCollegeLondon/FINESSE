2dae533d04983689c0de42fa5fc9b8de058dd2a9
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-10-29 12:51:55 +0000

    :recycle: Use global version in schema.

--- finesse/gui/measure_script/script.py
+++ finesse/gui/measure_script/script.py
@@ -15,7 +15,7 @@ from typing import Any
 import yaml
 from pubsub import pub
 from PySide6.QtWidgets import QWidget
-from schema import And, Or, Schema, SchemaError
+from schema import And, Const, Or, Schema, SchemaError
 from statemachine import State, StateMachine
 
 from finesse.config import ANGLE_PRESETS, SPECTROMETER_TOPIC, STEPPER_MOTOR_TOPIC
@@ -109,7 +109,10 @@ def parse_script(script: str | TextIOBase) -> dict[str, Any]:
 
     schema = Schema(
         {
-            "version": CURRENT_SCRIPT_VERSION,
+            "version": Const(
+                CURRENT_SCRIPT_VERSION,
+                f"Current script version number must be {CURRENT_SCRIPT_VERSION}",
+            ),
             "repeats": measurements_type,
             "sequence": And(
                 nonempty_list,
